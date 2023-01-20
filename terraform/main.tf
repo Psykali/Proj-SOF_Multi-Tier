627db3dd792f451ebcae095c4e63aac8972d019c
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-01-20 10:22:25 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,129 +1,66 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.39.1"
+# Generate random resource group name
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+resource "random_id" "log_analytics_workspace_name_suffix" {
+  byte_length = 8
+}
+
+resource "azurerm_log_analytics_workspace" "test" {
+  location            = var.log_analytics_workspace_location
+  # The WorkSpace name has to be unique across the whole of azure;
+  # not just the current subscription/tenant.
+  name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = var.log_analytics_workspace_sku
+}
+
+resource "azurerm_log_analytics_solution" "test" {
+  location              = azurerm_log_analytics_workspace.test.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  solution_name         = "ContainerInsights"
+  workspace_name        = azurerm_log_analytics_workspace.test.name
+  workspace_resource_id = azurerm_log_analytics_workspace.test.id
+
+  plan {
+    product   = "OMSGallery/ContainerInsights"
+    publisher = "Microsoft"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  location            = azurerm_resource_group.rg.location
+  name                = var.cluster_name
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+  tags                = {
+    Environment = "Development"
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.agent_count
+  }
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = file(var.ssh_public_key)
     }
   }
-}
-
-provider "azurerm" {
-  # Configuration options
-  features {}
-}
-
-#########################
-##Create Resource Group##
-#########################
-resource "azurerm_resource_group" "G10B12_GrittyRazors_SK" {
-  name     = "PERSO_SIEF"
-  location = "West Europe"
-}
-##########################
-##Create Storage account##
-##########################
-resource "azurerm_storage_account" "G10B12_GrittyRazors_SK" {
-  resource_group_name      = azurerm_resource_group.G10B12_GrittyRazors_SK.name
-  name                  = "persosiefsa"
-  location                 = azurerm_resource_group.G10B12_GrittyRazors_SK.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-##Create storage account container
-resource "azurerm_storage_container" "G10B12_GrittyRazors_SK" {
-  name                  = "persosiefb"
-  storage_account_name  = azurerm_storage_account.G10B12_GrittyRazors_SK.name
-  container_access_type = "blob"
-}
-##########################################
-##Create Application Insights for WebAPP##
-##########################################
-#resource "azurerm_application_insights" "G10B12_GrittyRazors_SK" {
-#  name                = "G10-B12-SK-appinsight"
-#  location            = azurerm_resource_group.G10B12_GrittyRazors_SK.location
-#  resource_group_name = azurerm_resource_group.G10B12_GrittyRazors_SK.name
-#  application_type    = "web"
-#}
-########################
-##Create Service plan ##
-########################
-resource "azurerm_service_plan" "G10B12_GrittyRazors_SK" {
-  name                = "perso-sief-splan"
-  location            = azurerm_resource_group.G10B12_GrittyRazors_SK.location
-  resource_group_name = azurerm_resource_group.G10B12_GrittyRazors_SK.name
-  os_type             = "Linux"
-#  os_type             = "Windows"
-  sku_name            = "P1v2"
-}
-#######################
-##Create Function App##
-#######################
-resource "azurerm_linux_function_app" "G10B12_GrittyRazors_SK" {
-  name                       = "perso-sief-FunApp"
-  location                   = azurerm_resource_group.G10B12_GrittyRazors_SK.location
-  resource_group_name        = azurerm_resource_group.G10B12_GrittyRazors_SK.name
-  service_plan_id            = azurerm_service_plan.G10B12_GrittyRazors_SK.id
-  storage_account_name       = azurerm_storage_account.G10B12_GrittyRazors_SK.name
-  storage_account_access_key = azurerm_storage_account.G10B12_GrittyRazors_SK.primary_access_key
-
-site_config {
-    always_on = true
-    application_stack {
-      dotnet_version = "6.0"
-    }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
   }
-  app_settings = { 
-  }
-
-}
-##Create Function App slot-Dev
-resource "azurerm_linux_function_app_slot" "G10B12_GrittyRazors_SK" {
-  name                       = "dev"
-  function_app_id            = azurerm_linux_function_app.G10B12_GrittyRazors_SK.id
-  storage_account_name       = azurerm_storage_account.G10B12_GrittyRazors_SK.name
-
-site_config {
-    always_on = true
-    application_stack {
-      dotnet_version = "6.0"
-    }
-  }
-   app_settings = {
-   
-  }
-}
-###################
-##Create Web-App ##
-###################
-resource "azurerm_linux_web_app" "G10B12_GrittyRazors_SK" {
-  name                = "Gperos-sief-webapp"
-  location            = azurerm_resource_group.G10B12_GrittyRazors_SK.location
-  resource_group_name = azurerm_resource_group.G10B12_GrittyRazors_SK.name
-  service_plan_id     = azurerm_service_plan.G10B12_GrittyRazors_SK.id
-
-site_config {
-    always_on = true
-    application_stack {
-      dotnet_version = "6.0"
-    }
-  }
-  app_settings = { 
-    
-  }
-}
-
-##Create Web-App slot-Dev
-resource "azurerm_linux_web_app_slot" "G10B12_GrittyRazors_SK" {
-  name                = "dev"
-  app_service_id      = azurerm_linux_web_app.G10B12_GrittyRazors_SK.id
-
-site_config {
-    always_on = true
-    application_stack {
-      dotnet_version = "6.0"
-    }
-  }
-   app_settings = {
-   
+  service_principal {
+    client_id     = var.aks_service_principal_app_id
+    client_secret = var.aks_service_principal_client_secret
   }
 }
