241639ef7982ae476d18d0dcb93cf4cda6a6218e
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-01-20 15:42:07 +0000

    Updated providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,8 @@
 terraform {
-  required_version = ">=1.0"
-
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      source = "hashicorp/azurerm"
+      version = "3.40.0"
     }
   }
 }
