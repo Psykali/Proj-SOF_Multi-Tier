fd37c382c6a01009cb7b92731efa14e36f59301d
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-01-23 15:51:35 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,16 +19,16 @@ resource "azurerm_kubernetes_cluster" "example" {
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
-  sensitive = true
-}
+#output "client_certificate" {
+#  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+#  sensitive = true
+#}
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
-
-  sensitive = true
-}
+#output "kube_config" {
+#  value = azurerm_kubernetes_cluster.example.kube_config_raw
+#
+#  sensitive = true
+#}
 
 resource "null_resource" "deploy-yaml" {
 
