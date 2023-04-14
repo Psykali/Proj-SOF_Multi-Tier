cf0f0f99fd002cb816e7747d60bf976620600769
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-04-14 14:41:58 +0000

    Updated main.tf

--- B18-PGsqlVPN/main.tf
+++ B18-PGsqlVPN/main.tf
@@ -11,24 +11,27 @@ provider "azurerm" {
   features {}
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
-  location = "West Europe"
-}
+## Create a resource group
+#resource "azurerm_resource_group" "example" {
+#  name     = "example-resource-group"
+#  location = "West Europe"
+#}
 
 # Create a virtual network
 resource "azurerm_virtual_network" "example" {
   name                = "example-virtual-network"
   address_space       = ["10.1.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location = "West Europe"
+##  location            = azurerm_resource_group.example.location
+  resource_group_name = "PERSO_SIEF"
+##  resource_group_name = azurerm_resource_group.example.name
 }
 
 # Create a subnet for the database
 resource "azurerm_subnet" "database" {
   name                 = "database-subnet"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = "PERSO_SIEF"
+##  resource_group_name = azurerm_resource_group.example.name
   address_prefixes     = ["10.1.1.0/24"]
   virtual_network_name = azurerm_virtual_network.example.name
 }
@@ -36,7 +39,8 @@ resource "azurerm_subnet" "database" {
 # Create a subnet for the VPN gateway
 resource "azurerm_subnet" "gateway" {
   name                 = "gateway-subnet"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = "PERSO_SIEF"
+##  resource_group_name = azurerm_resource_group.example.name
   address_prefixes     = ["10.1.2.0/24"]
   virtual_network_name = azurerm_virtual_network.example.name
 }
@@ -44,16 +48,20 @@ resource "azurerm_subnet" "gateway" {
 # Create a public IP address for the VPN gateway
 resource "azurerm_public_ip" "vpn_gateway" {
   name                = "vpn-gateway-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location = "West Europe"
+##  location            = azurerm_resource_group.example.location
+  resource_group_name = "PERSO_SIEF"
+##  resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
 }
 
 # Create the VPN gateway
 resource "azurerm_virtual_network_gateway" "vpn_gateway" {
   name                = "vpn-gateway"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location = "West Europe"
+##  location            = azurerm_resource_group.example.location
+  resource_group_name = "PERSO_SIEF"
+##  resource_group_name = azurerm_resource_group.example.name
   type                = "Vpn"
   vpn_type            = "RouteBased"
   active_active       = false
@@ -70,8 +78,10 @@ resource "azurerm_virtual_network_gateway" "vpn_gateway" {
 # Create a Postgres SQL server
 resource "azurerm_postgresql_server" "example" {
   name                = "example-psqlserver"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location = "West Europe"
+##  location            = azurerm_resource_group.example.location
+  resource_group_name = "PERSO_SIEF"
+##  resource_group_name = azurerm_resource_group.example.name
 
   administrator_login          = "psqladmin"
   administrator_login_password = "H@Sh1CoR3!"
@@ -94,7 +104,8 @@ resource "azurerm_postgresql_server" "example" {
 resource "azurerm_postgresql_firewall_rule" "block_all" {
   name                = "block-all"
   server_name         = azurerm_postgresql_server.example.name
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = "PERSO_SIEF"
+##  resource_group_name = azurerm_resource_group.example.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
