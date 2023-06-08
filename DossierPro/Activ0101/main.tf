077672f8a32d4f550b53a9a34d264b529c34eb0b
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-06-08 14:41:52 +0000

    Added main.tf

---
+++ DossierPro/Activ0101/main.tf
@@ -0,0 +1,124 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "PERSO_SIEF"
+  location = "France Central"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "skvnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "sksnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "lb_ip" {
+  name                = "skpublicIPForLB"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "skloadBalancer"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  frontend_ip_configuration {
+    name                 = "skpublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.lb_ip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bepool" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "skBackEndAddressPool"
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "sknic${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "sktestConfiguration"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+
+    load_balancer_backend_address_pools_ids = [azurerm_lb_backend_address_pool.bepool.id]
+  }
+}
+
+resource "azurerm_availability_set" "avset" {
+  name                = "skavset"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count                 = 2
+  name                  = "skvm${count.index}"
+  location              = azurerm_resource_group.rg.location
+  availability_set_id   = azurerm_availability_set.avset.id
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [element(azurerm_network_interface.nic.*.id, count.index)]
+  size                  = "Standard_DS1_v2"
+
+  admin_username = "testadmin"
+  admin_password = "Password1234!"
+
+  disable_password_authentication = false
+
+  os_disk {
+    name                 = "skosdisk${count.index}"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
+
+resource "azurerm_mariadb_server" "db" {
+  name                = "skmariadbserver"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  administrator_login          = "testadmin"
+  administrator_login_password = "Password1234!"
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "10.3"
+
+  ssl_enforcement_enabled          = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+}
+
+resource "azurerm_mariadb_database" "wpdb" {
+  name                = "skwordpressdb"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mariadb_server.db.name
+  charset             = "utf8"
+  collation           = "utf8_general_ci"
+}
+
+output "wordpress_site_url" {
+  value = "${azurerm_public_ip.lb_ip.ip_address}"
+}
