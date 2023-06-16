2086f3bbe9e44ee980fe66e46dfd0a5b86e6cabe
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-06-16 14:42:59 +0000

    Updated main.tf

--- DossierPro/Activ0102_WebAPP-WP/MEPP/main.tf
+++ DossierPro/Activ0102_WebAPP-WP/MEPP/main.tf
@@ -6,18 +6,15 @@ resource "azurerm_resource_group" "skrg" {
 
 # Define the MariaDB server
 resource "azurerm_mariadb_server" "skdb" {
-  name                = "skdb"
-  location            = azurerm_resource_group.skrg.location
-  resource_group_name = azurerm_resource_group.skrg.name
-  sku_name            = "B_Gen5_1"
-  storage_profile {
-    storage_mb        = 51200
-    backup_retention_days = 7
-    geo_redundant_backup_enabled = false
-  }
-  administrator_login          = "skadmin"
-  administrator_login_password = "P@ssw0rd1234"
-  version                       = "10.4"
+  name                         = "skwp-db"
+  location                     = azurerm_resource_group.skrg.location
+  resource_group_name          = azurerm_resource_group.skrg.name
+  sku_name                     = "B_Gen5_1"
+  storage_mb                   = 5120
+  administrator_login          = var.db_admin_username
+  administrator_login_password = var.db_admin_password
+  version                      = "10.3"
+  ssl_enforcement_enabled      = true
 }
 
 # Define the WebApp for development
@@ -74,9 +71,13 @@ resource "azurerm_storage_account" "skstorage" {
 # Define the WorkBooks for monitoring
 resource "azurerm_monitor_log_profile" "sklog" {
   name                = "sklog"
-  location            = azurerm_resource_group.skrg.location
+  locations           = [azurerm_resource_group.skrg.location]
+  categories          = ["Write"]
+  retention_policy {
+    enabled = true
+    days    = 30
+  }
   resource_group_name = azurerm_resource_group.skrg.name
-  categories          = ["Write", "Delete", "Action"]
 }
 
 resource "azurerm_monitor_diagnostic_setting" "skdiag" {
@@ -139,42 +140,51 @@ resource "azurerm_mariadb_firewall_rule" "skdb_fw" {
 }
 
 resource "azurerm_app_service_certificate" "skssl" {
-  name                = "skssl"
+  name         = "skssl"
   resource_group_name = azurerm_resource_group.skrg.name
-  location            = azurerm_resource_group.skrg.location
-  hostname            = azurerm_app_service.skprod_webapp.default_site_hostname
-  pfx_blob            = filebase64("${path.module}/skssl.pfx")
-  password            = "P@ssw0rd1234!"
+  location     = azurerm_resource_group.skrg.location
+  host_names   = [azurerm_app_service.skprod_webapp.default_site_hostname]
+  pfx_password = var.skssl_password
+  pfx_blob     = filebase64("${path.module}/skssl.pfx")
 }
 
 resource "azurerm_app_service_custom_hostname_binding" "skprod_binding" {
-  name                = "skprod-binding"
-  hostname            = "hackeuse.com"
-  ssl_certificate_id  = azurerm_app_service_certificate.skssl.id
-  app_service_name    = azurerm_app_service.skprod_webapp.name
-  resource_group_name = azurerm_resource_group.skrg.name
+  hostname             = azurerm_app_service_custom_domain.skprod_domain.hostname
+  resource_group_name  = azurerm_resource_group.skrg.name
+  app_service_name     = azurerm_app_service.skprod_webapp.name
+  thumbprint           = azurerm_app_service_certificate.skssl.thumbprint
+  ssl_state            = "SniEnabled"
+  host_name_ssl_states = ["${azurerm_app_service.skprod_webapp.default_site_hostname} = \"SniEnabled\""]
 }
 
 # Set up backup and recovery
-resource "azurerm_backup_policy_mariadb" "skdb_policy" {
-  name                = "skdb-policy"
+resource "azurerm_mariadb_database" "skdb" {
+  name                = "skdb"
   resource_group_name = azurerm_resource_group.skrg.name
-  server_name         = azurerm_mariadb_server.skdb.name
-  backup_policy {
-    type = "Automated"
+  server_name         = azurerm_mariadb_server.skdb_server.name
+  charset             = "utf8"
+  collation           = "utf8_general_ci"
+
+  backup {
     retention_days = 7
-    time = "23:00"
+    geo_redundant_backup_enabled = true
   }
 }
 
-resource "azurerm_backup_schedule_mariadb" "skdb_schedule" {
-  name                = "skdb-schedule"
+resource "azurerm_backup_policy" "skdb_policy" {
+  name                = "skdb-policy"
   resource_group_name = azurerm_resource_group.skrg.name
-  server_name         = azurerm_mariadb_server.skdb.name
-  backup_schedule {
-    type = "Full"
-    frequency = "Weekly"
-    retention_weeks = 4
-    start_time = "23:00"
+
+  rule {
+    name = "DailyBackup"
+    type = "Daily"
+    time = "23:00"
+    retention_daily {
+      count = 7
+    }
+  }
+
+  target {
+    resource_id = azurerm_mariadb_database.skdb.id
   }
 }
