eef6acfcdf4fbd785afaa0e06733929977c924c8
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-06-19 14:51:35 +0000

    Updated main.tf

--- DossierPro/Activ0101_Infra-LAMPX/main.tf
+++ DossierPro/Activ0101_Infra-LAMPX/main.tf
@@ -73,9 +73,9 @@ resource "azurerm_subnet" "sk_subnet" {
   virtual_network_name = azurerm_virtual_network.sk_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 
-  tags = {
-    Name = "sk_subnet"
-  }
+#  tags = {
+#    Name = "sk_subnet"
+#  }
 }
 
 # Define the virtual network
@@ -101,22 +101,22 @@ resource "azurerm_lb" "sk_lb" {
     public_ip_address_id          = azurerm_public_ip.sk_public_ip.id
   }
 
-  backend_address_pool {
-    name = "sk_lb_backend_pool"
-  }
+#  backend_address_pool {
+#    name = "sk_lb_backend_pool"
+#  }
 
-  probe {
-    name                = "sk_lb_probe"
-    protocol            = "Http"
-    request_path        = "/"
-    port                = 80
-    interval_in_seconds = 5
-    number_of_probes    = 2
-  }
+#  probe {
+#    name                = "sk_lb_probe"
+#    protocol            = "Http"
+#    request_path        = "/"
+#    port                = 80
+#    interval_in_seconds = 5
+#    number_of_probes    = 2
+#  }
 
-  tags = {
-    Name = "sk_lb"
-  }
+#  tags = {
+#    Name = "sk_lb"
+#  }
 }
 
 # Define the public IP address
@@ -172,14 +172,14 @@ resource "azurerm_monitor_metric_alert" "sk_cpu_alert" {
     action_group_id = azurerm_action_group.sk_action_group.id
   }
 }
-
+#
 # Define the action group
-resource "azurerm_action_group" "sk_action_group" {
-  name                = "sk_action_group"
-  resource_group_name = azurerm_resource_group.sk_rg.name
-  short_name          = "sk_action_group"
-  email_receiver {
-    name          = "sk_email_receiver"
-    email_address = "admin@example.com"
-  }
-}
+#resource "azurerm_action_group" "sk_action_group" {
+#  name                = "sk_action_group"
+#  resource_group_name = azurerm_resource_group.sk_rg.name
+#  short_name          = "sk_action_group"
+#  email_receiver {
+#    name          = "sk_email_receiver"
+#    email_address = "admin@example.com"
+#  }
+#}
