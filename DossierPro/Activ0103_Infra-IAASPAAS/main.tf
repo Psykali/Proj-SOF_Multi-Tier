4e347a242633d091767dfcdf1c54f209f90a1814
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-06-21 13:21:54 +0000

    Updated main.tf

--- DossierPro/Activ0103_Infra-IAASPAAS/main.tf
+++ DossierPro/Activ0103_Infra-IAASPAAS/main.tf
@@ -28,6 +28,26 @@ resource_group_name = azurerm_resource_group.rg.name
 allocation_method = "Static"
 }
 
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  name                = "sk-backend-pool"
+  loadbalancer_id     = azurerm_lb.lb.id
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "sk-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+
+  backend_address_pool_ids = [
+    azurerm_lb_backend_address_pool.backend_pool.id
+  ]
+}
+
 ##Load Balancer
 resource "azurerm_lb" "lb" {
 name = "sk-lb"
@@ -78,42 +98,43 @@ private_ip_address_allocation = "Dynamic"
 }
 }
 
-##MariaDB Virtual Machine
 resource "azurerm_linux_virtual_machine" "mariadb_vm" {
-name = "sk-mariadb-vm"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-size = "Standard_B2s"
-admin_username = "adminuser"
-disable_password_authentication = true
-network_interface_ids = [
-azurerm_network_interface.nic.id
-]
+  name                          = "sk-mariadb-vm"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  size                          = "Standard_B2s"
+  admin_username                = "adminuser"
+  disable_password_authentication = false
 
-admin_ssh_key {
-username = "adminuser"
-public_key = file("~/.ssh/id_rsa.pub")
-}
+  os_profile {
+    computer_name  = "sk-mariadb-vm"
+    admin_username = "adminuser"
+    admin_password = "your_admin_password"
+  }
 
-os_disk {
-name = "sk-mariadb-vm-osdisk"
-caching = "ReadWrite"
-storage_account_type = "Standard_LRS"
-}
+  os_disk {
+    name              = "sk-mariadb-vm-osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-source_image_reference {
-publisher = "mariadb"
-offer = "mariadb-10-4"
-sku = "mariadb-10-4"
-version = "latest"
-}
+  source_image_reference {
+    publisher = "mariadb"
+    offer     = "mariadb-10-4"
+    sku       = "mariadb-10-4"
+    version   = "latest"
+  }
 
-custom_data = base64encode(
-<<-EOF
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y mariadb-server
-sudo mysql_secure_installation
-EOF
-)
+  custom_data = base64encode(
+    <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y mariadb-server
+    sudo mysql_secure_installation
+    EOF
+  )
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
 }
