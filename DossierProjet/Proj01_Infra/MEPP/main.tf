1a2b61083650f6a7aebad499d7a92e2cf2d74ae3
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-03 13:17:15 +0000

    Updated main.tf

--- DossierProjet/Proj01_Infra/MEPP/main.tf
+++ DossierProjet/Proj01_Infra/MEPP/main.tf
@@ -1,112 +1,214 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "${var.azurerm_version}"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "skprj01_rg" {
-  name     = "${var.resource_group_name}"
-  location = "${var.resource_group_location}"
+resource "azurerm_resource_group" "rg" {
+  name     = "PERSO_SIEF"
+  location = "francecentral"
 }
 
-resource "azurerm_virtual_network" "skprj01_vnet" {
-  name                = "${var.virtual_network_name}"
-  location            = "${azurerm_resource_group.skprj01_rg.location}"
-  resource_group_name = "${azurerm_resource_group.skprj01_rg.name}"
-  address_space       = "${var.virtual_network_address_space}"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "skVNET"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "skprj01_subnet" {
-  name                 = "${var.subnet_name}"
-  resource_group_name  = "${azurerm_resource_group.skprj01_rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.skprj01_vnet.name}"
-  address_prefixes     = "${var.subnet_address_prefixes}"
+resource "azurerm_subnet" "subnet" {
+  name                 = "skSubnet"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-module "mariadb_vm" {
-  source              = "Azure/compute/azurerm"
-  version             = "1.2.0"
-  location            = "${azurerm_resource_group.skprj01_rg.location}"
-  resource_group_name = "${azurerm_resource_group.skprj01_rg.name}"
-  vm_os_simple        = "${var.mariadb_vm_os_simple}"
-  public_ip_dns       = ["${var.mariadb_vm_public_ip_dns}"]
-  vnet_subnet_id      = "${azurerm_subnet.skprj01_subnet.id}"
-}
+resource "azurerm_network_interface" "nic" {
+  name                = "skNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-resource "azurerm_lb" "skprj01_lb" {
-  name                = "${var.load_balancer_name}"
-  location            = "${azurerm_resource_group.skprj01_rg.location}"
-  resource_group_name = "${azurerm_resource_group.skprj01_rg.name}"
-
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = "${azurerm_public_ip.skprj01_pip.id}"
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_public_ip" "skprj01_pip" {
-  name                = "${var.public_ip_name}"
-  location            = "${azurerm_resource_group.skprj01_rg.location}"
-  resource_group_name = "${azurerm_resource_group.skprj01_rg.name}"
-  allocation_method   = "${var.public_ip_allocation_method}"
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "skVM"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_B1s"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  admin_username      = "skadminadmin"
+  admin_password      = "skpassadminpass"
 }
 
-resource "azurerm_lb_backend_address_pool" "skprj01_backend_pool" {
-  name                = "skprj01_backend_pool"
-  loadbalancer_id     = "${azurerm_lb.skprj01_lb.id}"
+resource "azurerm_mariadb_server" "mariadb" {
+  name                = "skMariaDB"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  administrator_login          = "skadminadmin"
+  administrator_login_password = "skpassadminpass"
+
+  sku_name   = "B_Gen5_2"
+  storage_mb = 5120
+  version    = "10.3"
+
 }
 
-resource "azurerm_app_service_plan" "skprj01_asp" {
-  name                = "${var.app_service_plan_name}"
-  location            = "${azurerm_resource_group.skprj01_rg.location}"
-  resource_group_name = "${azurerm_resource_group.skprj01_rg.name}"
+resource "azurerm_app_service_plan" "asp" {
+  name                = "skASP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   sku {
-    tier ="${var.app_service_plan_sku_tier}"
-    size = "${var.app_service_plan_sku_size}"
-  }
+    tier     ="Standard"
+    size     ="S1"
+    capacity ="2"
+}
 }
 
-resource "azurerm_app_service" "skprj01_webapp" {
-  count               = "${var.app_service_count}"
-  name                = "skprj01_webapp_${count.index}"
-  location            = "${azurerm_resource_group.skprj01_rg.location}"
-  resource_group_name = "${azurerm_resource_group.skprj01_rg.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.skprj01_asp.id}"
+resource "azurerm_app_service" "webapp1" {
+   name                ="skWebApp1"
+   location            ="francecentral"
+   resource_group_name ="PERSO_SIEF"
 
-  site_config {
-    always_on = true
-  }
+   app_service_plan_id=azurerm_app_service_plan.asp.id
 
-  app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.skprj01_ai.instrumentation_key}"
-  }
+   site_config{
+      always_on=true
+      dotnet_framework_version="v4.0"
+      scm_type="LocalGit"
 
-  connection_string {
-    name  = "mariadb_connection"
-    type  = "MySQL"
-    value = "Server=${module.mariadb_vm.vm_public_ip};Database=mydb;Uid=myuser;Pwd=mypassword;"
-  }
+   }
 }
 
-resource "azurerm_storage_account" "skprj01_sa" {
-  name                     = "${var.storage_account_name}"
-  resource_group_name      = "${azurerm_resource_group.skprj01_rg.name}"
-  location                 = "${azurerm_resource_group.skprj01_rg.location}"
-  account_tier             = "${var.storage_account_tier}"
-  account_replication_type = "${var.storage_account_replication_type}"
+resource "azurerm_app_service" "webapp2" {
+   name                ="skWebApp2"
+   location            ="francecentral"
+   resource_group_name ="PERSO_SIEF"
+
+   app_service_plan_id=azurerm_app_service_plan.asp.id
+
+   site_config{
+      always_on=true
+      dotnet_framework_version="v4.0"
+      scm_type="LocalGit"
+
+   }
 }
 
-resource "azurerm_application_insights" "skprj01_ai" {
-  name                = "${var.application_insights_name}"
-  location            = "${azurerm_resource_group.skprj01_rg.location}"
-  resource_group_name = "${azurerm_resource_group.skprj01_rg.name}"
-  application_type    = "${var.application_insights_type}"
+resource "azurerm_storage_account" "storageaccount" {
+   name                     ="skstorageaccount${random_string.random_string.result}"
+   resource_group_name      ="PERSO_SIEF"
+   location                 ="francecentral"
+
+   account_tier             ="Standard"
+   account_replication_type ="GRS"
+
+}
+
+resource "random_string" "random_string"{
+length=8
+special=false
+upper=false
+}
+
+
+resource "azurerm_application_insights" "appinsights1"{
+name="skAppInsights1"
+location="francecentral"
+resource_group_name="PERSO_SIEF"
+
+application_type="web"
+
+}
+
+
+resource "azurerm_application_insights" "appinsights2"{
+name="skAppInsights2"
+location="francecentral"
+resource_group_name="PERSO_SIEF"
+
+application_type="web"
+
+}
+
+resource "azurerm_loadbalancer" "loadbalancer"{
+name="skLoadBalancer"
+location="francecentral"
+resource_group_name="PERSO_SIEF"
+
+frontend_ip_configuration{
+name="LoadBalancerFrontEnd"
+public_ip_address_id=azurerm_public_ip.publicip.id
+}
+}
+
+resource "azurerm_public_ip" "publicip"{
+name="skPublicIP"
+location="francecentral"
+resource_group_name="PERSO_SIEF"
+
+allocation_method="Static"
+sku="Standard"
+}
+
+resource "azurerm_lb_backend_address_pool" "backendpool"{
+loadbalancer_id=azurerm_loadbalancer.loadbalancer.id
+name="BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "probe"{
+loadbalancer_id=azurerm_loadbalancer.loadbalancer.id
+name="httpProbe"
+port=80
+protocol="Http"
+request_path="/"
+
+}
+
+resource "azurerm_lb_rule" "rule"{
+loadbalancer_id=azurerm_loadbalancer.loadbalancer.id
+name="httpRule"
+
+protocol="Tcp"
+frontend_port=80
+backend_port=80
+
+frontend_ip_configuration_name="LoadBalancerFrontEnd"
+backend_address_pool_id=azurerm_lb_backend_address_pool.backendpool.id
+probe_id=azurerm_lb_probe.probe.id
+
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "association1"{
+network_interface_id=azurerm_network_interface.nic1.id
+ip_configuration_name="internal"
+backend_address_pool_id=azurerm_lb_backend_address_pool.backendpool.id
+
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "association2"{
+network_interface_id=azurerm_network_interface.nic2.id
+ip_configuration_name="internal"
+backend_address_pool_id=azurerm_lb_backend_address_pool.backendpool.id
+
 }
