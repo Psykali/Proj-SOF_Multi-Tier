4bd27e210713e02c352b2817550a10fc3ac5f251
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-04 14:52:02 +0000

    Updated main.tf

--- DossierPro/Activ0101_Infra-LAMPX/main.tf
+++ DossierPro/Activ0101_Infra-LAMPX/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 
 # Define the virtual network
 resource "azurerm_virtual_network" "sk_vnet" {
-  name                = "sk_vnet"
+  name                = "sk_vnet_active0101"
   address_space       = ["10.0.0.0/16"]
   location            = "francecentral"
   resource_group_name = "PERSO_SIEF"
@@ -14,7 +14,7 @@ resource "azurerm_virtual_network" "sk_vnet" {
 
 # Define the subnet
 resource "azurerm_subnet" "sk_subnet" {
-  name                 = "sk_subnet"
+  name                 = "sk_subnet_active0101"
   resource_group_name  = "PERSO_SIEF"
   virtual_network_name = azurerm_virtual_network.sk_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "sk_subnet" {
 
 # Define the public IP address
 resource "azurerm_public_ip" "sk_public_ip" {
-  name                = "sk_public_ip"
+  name                = "sk_public_ip_active0101"
   location            = "francecentral"
   resource_group_name = "PERSO_SIEF"
   allocation_method   = "Static"
@@ -30,7 +30,7 @@ resource "azurerm_public_ip" "sk_public_ip" {
 
 # Define the load balancer
 resource "azurerm_lb" "sk_lb" {
-  name                = "sk_lb"
+  name                = "sk_lb_active0101"
   location            = "francecentral"
   resource_group_name = "PERSO_SIEF"
 
@@ -42,13 +42,13 @@ resource "azurerm_lb" "sk_lb" {
 
 # Define the load balancer backend address pool
 resource "azurerm_lb_backend_address_pool" "sk_lb_backend_pool" {
-  name                = "sk_lb_backend_pool"
+  name                = "sk_lb_backend_pool_active0101"
   loadbalancer_id     = azurerm_lb.sk_lb.id
 }
 
 # Define the load balancer rule
 resource "azurerm_lb_rule" "sk_lb_rule" {
-  name                   = "sk_lb_rule"
+  name                   = "sk_lb_rule_active0101"
   frontend_ip_configuration_name = azurerm_lb.sk_lb.frontend_ip_configuration[0].name
   loadbalancer_id        = azurerm_lb.sk_lb.id
   protocol               = "Tcp"
@@ -59,7 +59,7 @@ resource "azurerm_lb_rule" "sk_lb_rule" {
 
 # Define the virtual machines
 resource "azurerm_linux_virtual_machine" "sk_vm" {
-  name                  = "skvm"
+  name                  = "skvmactive0101"
   location              = "francecentral"
   resource_group_name   = "PERSO_SIEF"
   size                  = "Standard_B1s"
@@ -69,7 +69,7 @@ resource "azurerm_linux_virtual_machine" "sk_vm" {
   network_interface_ids = [azurerm_network_interface.sk_nic.id]
 
   os_disk {
-    name              = "sk_vm_osdisk"
+    name              = "sk_vm_osdisk_active0101"
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -92,23 +92,23 @@ resource "azurerm_linux_virtual_machine" "sk_vm" {
               )
 
   tags = {
-    Name = "sk_vm"
+    Name = "sk_vm_active0101"
   }
 }
 
 # Define the network interface
 resource "azurerm_network_interface" "sk_nic" {
-  name                = "sk_nic"
+  name                = "sk_nic_active0101"
   location            = "francecentral"
   resource_group_name = "PERSO_SIEF"
 
   ip_configuration {
-    name                          = "sk_nic_ipconfig"
+    name                          = "sk_nic_ipconfig_active0101"
     subnet_id                     = azurerm_subnet.sk_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 
   tags = {
-    Name = "sk_nic"
+    Name = "sk_nic_active0101"
   }
 }
