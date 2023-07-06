d31d0f07e778dc14f089d8f4f6e0b73437c11d7a
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-06 14:59:15 +0000

    Added variable.tf

---
+++ X04-Activ01-02-WebAPP-WP/variable.tf
@@ -0,0 +1,32 @@
+# Define variables
+variable "resource_group_name" {
+  default = "PERSO_SIEF"
+}
+
+variable "location" {
+  default = "francecentral"
+}
+
+variable "vm_name" {
+  default = "sk-lamp-vm"
+}
+
+variable "vm_size" {
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  default = "adminuser"
+}
+
+variable "admin_password" {
+  default = "P@ssw0rd123"
+}
+
+variable "app_insights_name" {
+  default = "skappinsights"
+}
+
+variable "app_insights_instrumentation_key" {
+  type = string
+}
