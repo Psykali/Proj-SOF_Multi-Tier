fadcd1d2ca6e9b53a643dedbc29ad9e230e628ba
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-06 09:53:12 +0000

    Updated main.tf

--- X01-DP-ContainerReg/main.tf
+++ X01-DP-ContainerReg/main.tf
@@ -6,18 +6,22 @@ resource "azurerm_container_registry" "acr" {
   name                = "skcontreg"
   resource_group_name = "PERSO_SIEF"
   location            = "francecentral"
-  sku                 = "Premium"
-  admin_enabled       = false
 
-  replication {
-    location                = "westeurope"
-    zone_redundancy_enabled = true
+  sku {
+    name = "Premium"
+
+    replication {
+      location                = "westeurope"
+      zone_redundancy_enabled = true
+    }
+
+    replication {
+      location                = "northeurope"
+      zone_redundancy_enabled = true
+    }
   }
 
-  replication {
-    location                = "northeurope"
-    zone_redundancy_enabled = true
-  }
+  admin_enabled = false
 
   tags = {
     environment = "production"
