786c3c8eb0bd881b3dac723c01a2f9cafa09b0ca
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-06 14:21:48 +0000

    Updated main.tf

--- X03-Activ01-01-LAMPx/main.tf
+++ X03-Activ01-01-LAMPx/main.tf
@@ -4,6 +4,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group_name
+}
 
 # Create subnet
 resource "azurerm_subnet" "subnet" {
