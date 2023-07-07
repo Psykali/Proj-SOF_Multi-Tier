7bfe30288b9ce9095edaa5e418c7d9670ae24293
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-07 15:02:24 +0000

    Updated main.tf

--- X01-DP-ContainerReg/main.tf
+++ X01-DP-ContainerReg/main.tf
@@ -6,14 +6,14 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-resource "null_resource" "acr_import" {
-  provisioner "local-exec" {
-    command = <<EOT
-      az login --service-principal -u $${ARM_CLIENT_ID} -p $${ARM_CLIENT_SECRET} --tenant $${ARM_TENANT_ID} 
-      az acr login --name ${azurerm_container_registry.acr.name}
-      az acr import --name ${azurerm_container_registry.acr.name} --source docker.io/library/wordpress:latest --image wordpress:latest
-      az acr import --name ${azurerm_container_registry.acr.name} --source docker.io/rocketchat/rocket.chat:latest --image rocketchat:latest
-      az acr import --name ${azurerm_container_registry.acr.name} --source docker.io/psykali/stackoverp20kcab:latest --image psykali/stackoverp20kcab:latest
-    EOT
-  }
-}
+#resource "null_resource" "acr_import" {
+#  provisioner "local-exec" {
+#    command = <<EOT
+#      az login --service-principal -u $${ARM_CLIENT_ID} -p $${ARM_CLIENT_SECRET} --tenant $${ARM_TENANT_ID} 
+#      az acr login --name ${azurerm_container_registry.acr.name}
+#      az acr import --name ${azurerm_container_registry.acr.name} --source docker.io/library/wordpress:latest --image wordpress:latest
+#      az acr import --name ${azurerm_container_registry.acr.name} --source docker.io/rocketchat/rocket.chat:latest --image rocketchat:latest
+#      az acr import --name ${azurerm_container_registry.acr.name} --source docker.io/psykali/stackoverp20kcab:latest --image psykali/stackoverp20kcab:latest
+#    EOT
+#  }
+#}
