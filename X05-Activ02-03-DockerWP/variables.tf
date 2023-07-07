6879439001ee9cc0c2de57722b11836ec1236e7e
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-07 15:44:07 +0000

    Added variables.tf

---
+++ X05-Activ02-03-DockerWP/variables.tf
@@ -0,0 +1,23 @@
+variable "location" {
+  default = "westeurope"
+}
+
+variable "resource_group_name" {
+  default = "PERSO_SIEF"
+}
+
+variable "admin_username" {
+  default = "skwp_admin"
+}
+
+variable "admin_password" {
+  default = "P@ssw0rd123"
+}
+
+variable "sql_server_name" {
+  default = "skwpp20_sqlserver"
+}
+
+variable "sql_database_name" {
+  default = "skwp_sqldb"
+}
