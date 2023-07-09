dbbb4e8515b8a67118c0d3bdb6279103561cef66
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-09 15:56:32 +0000

    Added variables.tf

---
+++ XYZ-ContainerReg-OK/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  type        = string
+  description = "PERSO_SIEF"
+}
+
+variable "location_rg" {
+  type        = string
+  description = "francecentral"
+}
+
+variable "location_contreg" {
+  type        = string
+  description = "francecentral"
+}
