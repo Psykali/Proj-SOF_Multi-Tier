51b9255e6465e3070996427699e2952e48dc075d
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-09 18:13:01 +0000

    Added variables.tf

---
+++ X02-Docker-RocketChat/variables.tf
@@ -0,0 +1,47 @@
+variable "location" {
+  default = "westeurope"
+}
+
+variable "resource_group_name" {
+  default = "PERSO_SIEF"
+}
+
+variable "admin_username" {
+  default = "skwp_admin"
+}
+
+variable "admin_password" {
+  default = "P@ssw0rd123"
+}
+
+variable "sql_server_name" {
+  default = "skwpp20-sqlserver"
+}
+
+variable "sql_server_fqdn" {
+  default = "skwpp20-sqlserver.database.windows.net:1433"
+}
+
+variable "sql_database_name" {
+  default = "skwp-sqldb"
+}
+
+variable "container_name" {
+  default = "skdwpp20cont"
+}
+
+variable "image_name" {
+  default = "skp20contreg.azurecr.io/wordpress:latest"
+}
+
+##variable "scope_map_token_name" {
+##  default = "docker login -u Admin -p Zxq1EiAjiDl3BEDNbRXBzrjN7szlmJ+DLtdFe63Vn4+ACRCPwHZC skp20contreg.azurecr.io"
+##}
+
+variable "scope_map_token_name" {
+  default = "Admin"
+}
+
+variable "scope_map_token_password" {
+  default = "Zxq1EiAjiDl3BEDNbRXBzrjN7szlmJ+DLtdFe63Vn4+ACRCPwHZC"
+}
