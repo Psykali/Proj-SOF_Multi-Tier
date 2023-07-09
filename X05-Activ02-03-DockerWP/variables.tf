8898763c955b171486e07b7a10ecb57cfb35552c
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-09 15:16:12 +0000

    Updated variables.tf

--- X05-Activ02-03-DockerWP/variables.tf
+++ X05-Activ02-03-DockerWP/variables.tf
@@ -29,3 +29,7 @@ variable "container_name" {
 variable "image_name" {
   default = "skp20contreg.azurecr.io/wordpress:latest"
 }
+
+variable "scope_map_token_name" {
+  default = "docker login -u Admin -p Zxq1EiAjiDl3BEDNbRXBzrjN7szlmJ+DLtdFe63Vn4+ACRCPwHZC skp20contreg.azurecr.io"
+}
