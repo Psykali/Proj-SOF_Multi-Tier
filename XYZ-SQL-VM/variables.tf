2a08a892ceda3fde4b24d67fbcb844505c150a5a
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-09 17:32:20 +0000

    Added variables.tf

---
+++ XYZ-SQL-VM/variables.tf
@@ -0,0 +1,35 @@
+variable "location" {
+  default = "westeurope"
+}
+
+variable "resource_group_name" {
+  default = "PERSO_SIEF"
+}
+
+variable "admin_username" {
+  default = "skwp_admin"
+}
+
+variable "admin_password" {
+  default = "P@ssw0rd123P@ssw0rd123"
+}
+
+variable "ubuntu-vm" {
+  default = "skwpp20mdb"
+}
+
+variable "subnet" {
+  default = "skwpp20subnet"
+}
+
+variable "virtual_network" {
+  default = "skwpp20vnet"
+}
+
+variable "network_interface" {
+  default = "skwpp20nic"
+}
+
+variable "ubuntu-pip" {
+  default = "skwpp20pip"
+}
