204394eca320b8de22300b605cf73eaa447f8bf0
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-09 15:48:57 +0000

    Added variables.tf

---
+++ XYZ-StorageAcc-KeyVault/variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  type        = string
+  description = "PERSO_SIEF"
+}
+
+variable "location" {
+  type        = string
+  description = "francecentral"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "skpersop20sa"
+}
+
+variable "blob_container_name" {
+  type        = string
+  description = "helloblob"
+}
+
+variable "key_vault_name" {
+  type        = string
+  description = "skpersokv"
+}
+
+variable "key_vault_secret_name" {
+  type        = string
+  description = "hellovault"
+}
