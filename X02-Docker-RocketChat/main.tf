a2b538e608492e9ceaffd4b55fe3d12d3e00b00f
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-09 18:23:44 +0000

    Added main.tf

---
+++ X02-Docker-RocketChat/main.tf
@@ -0,0 +1,41 @@
+###########################################################
+### Create the resource group
+##resource "azurerm_resource_group" "rg" {
+##  name     = var.resource_group_name
+##  location = var.location
+##}
+############################################################
+##resource "azurerm_container_registry" "acr" {
+##    name                = "rocketcatacr"
+##    resource_group_name = var.resource_group_name
+##    location            = var.location
+##    sku                 = "Basic"
+##    admin_enabled       = true
+##}
+#############################################################
+resource "azurerm_container_group" "cg" {
+    name                = "sk-rocketcat-cg"
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    ip_address_type     = "public"
+    dns_name_label      = "rocketcat-dns"
+    os_type             = "Linux"
+
+    container {
+        name   = "rocketcat"
+        image  = "skp20contreg.azurecr.io/rocketcat:latest"
+        cpu    = "0.5"
+        memory = "1.5"
+
+        ports {
+            port     = 80
+            protocol = "TCP"
+        }
+    }
+
+    image_registry_credential {
+    server   = "skp20contreg.azurecr.io"
+    username = var.scope_map_token_name
+    password = var.scope_map_token_password
+  }
+}
