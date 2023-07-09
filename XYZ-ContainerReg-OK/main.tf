fd13a97d80afb59fbd291c079d282fc84d1a2b07
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-09 15:55:01 +0000

    Updated main.tf

--- XYZ-ContainerReg-OK/main.tf
+++ XYZ-ContainerReg-OK/main.tf
@@ -1,3 +1,11 @@
+###########################################################
+### Create the resource group
+##resource "azurerm_resource_group" "rg" {
+##  name     = var.resource_group_name
+##  location = var.location
+##}
+############################################################
+# Create the container registry
 resource "azurerm_container_registry" "acr" {
   name                = "skP20ContReg"
   resource_group_name = "PERSO_SIEF"
