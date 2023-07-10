251dd11225d0144d70c7fe47a0fb64b3aa2542be
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-10 15:58:41 +0000

    Added main.tf

---
+++ X05-Activ02-03-DockerWP/main.tf
@@ -0,0 +1,84 @@
+###########################################################
+### Create the resource group
+##resource "azurerm_resource_group" "rg" {
+##  name     = var.resource_group_name
+##  location = var.location
+##}
+############################################################
+### Create the SQL Server
+##resource "azurerm_sql_server" "sql_server" {
+##  name                         = var.sql_server_name
+##  location                     = var.location
+##  resource_group_name          = var.resource_group_name
+##  version                      = "12.0"
+##  administrator_login          = var.admin_username
+##  administrator_login_password = var.admin_password
+##}
+##############################################################
+### Create firewall rule for SQL Server
+##resource "azurerm_sql_firewall_rule" "sql_firewall_rule" {
+##  name                = "AllowAll"
+##  resource_group_name = var.resource_group_name
+##  server_name         = azurerm_sql_server.sql_server.name
+##  start_ip_address    = "0.0.0.0"
+##  end_ip_address      = "255.255.255.255"
+##}
+##############################################################
+### Create SQL Database
+##resource "azurerm_sql_database" "sql_database" {
+##  name                = var.sql_database_name
+##  location            = var.location
+##  resource_group_name = var.resource_group_name
+##  server_name         = azurerm_sql_server.sql_server.name
+##  edition             = "GeneralPurpose"
+##  requested_service_objective_name = "GP_Gen5_2"
+##}
+###############################################################
+# Create the Azure Container Instance
+resource "azurerm_container_group" "aci" {
+  name                = var.container_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  ip_address_type     = "Public"
+  dns_name_label      = "${var.container_name}-dns"
+  os_type             = "Linux"
+
+  image_registry_credential {
+    server   = "skp20contreg.azurecr.io"
+    username = var.scope_map_token_name
+    password = var.scope_map_token_password
+  }
+
+  container {
+    name   = "${var.container_name}-1"
+    image  = var.image_name
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  container {
+    name   = "${var.container_name}-2"
+    image  = var.image_name
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
+##    environment_variables = {
+##      WORDPRESS_DB_HOST     = var.sql_server_fqdn
+##      WORDPRESS_DB_USER     = var.admin_username
+##      WORDPRESS_DB_PASSWORD = var.admin_password
+##      WORDPRESS_DB_NAME     = var.sql_database_name
+##      WORDPRESS_DB_SSL = "true"
+##    }
+  }
+}
