1a505dc25726285e1a508b5edf693c659359d33a
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-11 17:56:11 +0000

    Updated variables.tf

--- X02-IAAS-RocketChat/variables.tf
+++ X02-IAAS-RocketChat/variables.tf
@@ -1,35 +1,31 @@
-variable "location" {
-  default = "westeurope"
-}
-
 variable "resource_group_name" {
   default = "PERSO_SIEF"
 }
 
+variable "location" {
+  default = "francecentral"
+}
+
+variable "vnet_name" {
+  default = "skrcvnet"
+}
+
+variable "subnet_name" {
+  default = "skrcsubnet"
+}
+
+variable "vm_name" {
+  default = "skrcvm"
+}
+
+variable "vm_size" {
+  default = "Standard_B2s"
+}
+
 variable "admin_username" {
-  default = "skwp_admin"
+  default = "skrcadmin"
 }
 
 variable "admin_password" {
-  default = "P@ssw0rd123"
-}
-
-variable "container_name" {
-  default = "skdrcp20"
-}
-
-variable "image_name" {
-  default = "skp20contreg.azurecr.io/rocketchat:latest"
-}
-
-##variable "scope_map_token_name" {
-##  default = "docker login -u Admin -p Zxq1EiAjiDl3BEDNbRXBzrjN7szlmJ+DLtdFe63Vn4+ACRCPwHZC skp20contreg.azurecr.io"
-##}
-
-variable "scope_map_token_name" {
-  default = "Admin"
-}
-
-variable "scope_map_token_password" {
-  default = "Zxq1EiAjiDl3BEDNbRXBzrjN7szlmJ+DLtdFe63Vn4+ACRCPwHZC"
+  default = "P@P@ssw0rd123P@ssw0rd123"
 }
