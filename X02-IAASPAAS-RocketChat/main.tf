9f057fdd9f17a645fc4f74570e27d22fc0db070f
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-11 18:29:41 +0000

    Updated main.tf

--- X02-IAASPAAS-RocketChat/main.tf
+++ X02-IAASPAAS-RocketChat/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_virtual_network" "vnet" {
 
 # Create a subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet
+  name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
