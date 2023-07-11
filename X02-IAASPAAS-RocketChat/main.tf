77c95d98dadef6e42bfa4ff0e9fb7676ead7d94b
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-11 18:27:31 +0000

    Updated main.tf

--- X02-IAASPAAS-RocketChat/main.tf
+++ X02-IAASPAAS-RocketChat/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "${var.vm_name}-ipconfig"
-    subnet_id                     = data.azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
