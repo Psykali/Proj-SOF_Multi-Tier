211d23a955e221de3415620679b3faa0b33cb2b5
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-11 18:48:03 +0000

    Updated main.tf

--- X02-IAASPAAS-RocketChat/main.tf
+++ X02-IAASPAAS-RocketChat/main.tf
@@ -10,6 +10,7 @@ resource "azurerm_public_ip" "public_ip" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
+  domain_name_label   = var.vm_name
 }
 
 resource "azurerm_network_interface" "nic" {
