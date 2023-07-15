de6633b131a8d608c324802daf6623d80a379704
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-15 15:44:18 +0000

    Updated provider.tf

--- X03-Activ01-01-LAMPx/provider.tf
+++ X03-Activ01-01-LAMPx/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.40.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "PERSO_SIEF"
+    storage_account_name = "sppersotfstates"
+    container_name       = "lampxvirtminstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
