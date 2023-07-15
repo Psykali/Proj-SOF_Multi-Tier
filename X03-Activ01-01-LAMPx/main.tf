ae0220947bb6e57a4d63d982b85eca4b6a1f5237
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-15 12:47:26 +0000

    Updated main.tf

--- X03-Activ01-01-LAMPx/main.tf
+++ X03-Activ01-01-LAMPx/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = var.virtual_network_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = [var.address_space]
+  address_space       = ["10.0.0.0/8"]
 }
 
 # Create Network Interface
