128253fc198be75c1ec184e8282a10fdf1c35ef8
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-19 15:45:13 +0000

    Updated main.tf

--- X03-Activ01-01-LAMPx/main.tf
+++ X03-Activ01-01-LAMPx/main.tf
@@ -182,8 +182,6 @@ provisioner "remote-exec" {
     "cd /tmp",
     "curl -O https://wordpress.org/latest.tar.gz",
     "tar xzvf latest.tar.gz",
-    "sudo cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php",
-    "sudo sed -i 's/define( 'DB_HOST', 'localhost' );/define( 'DB_HOST', '127.0.0.1:3306' );/' /tmp/wordpress/wp-config.php",
     "sudo cp -a /tmp/wordpress/. /var/www/html/wordpress",
     "sudo chown -R www-data:www-data /var/www/html",
     "sudo sed -i 's/Listen 80/Listen 80\\nListen 8080/' /etc/apache2/ports.conf",
