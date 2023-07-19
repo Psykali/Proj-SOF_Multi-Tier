69e1b6481319e6dfaffd3dd1c6ffb432e51611f0
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-19 16:43:47 +0000

    Updated main.tf

--- OK-XYZ-ContainerReg/main.tf
+++ OK-XYZ-ContainerReg/main.tf
@@ -19,15 +19,3 @@ resource "azurerm_container_registry" "acr" {
     CreatedBy = "SK"
   }
 }
-
-#resource "null_resource" "acr_import" {
-#  provisioner "local-exec" {
-#    command = <<EOT
-#      az login --service-principal -u $${ARM_CLIENT_ID} -p $${ARM_CLIENT_SECRET} --tenant $${ARM_TENANT_ID} 
-#      az acr login --name ${azurerm_container_registry.acr.name}
-#      az acr import --name ${azurerm_container_registry.acr.name} --source docker.io/library/wordpress:latest --image wordpress:latest
-#      az acr import --name ${azurerm_container_registry.acr.name} --source docker.io/rocketchat/rocket.chat:latest --image rocketchat:latest
-#      az acr import --name ${azurerm_container_registry.acr.name} --source docker.io/psykali/stackoverp20kcab:latest --image psykali/stackoverp20kcab:latest
-#    EOT
-#  }
-#}
