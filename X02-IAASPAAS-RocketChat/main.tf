92c11c1ae9ea0acc23aa4f6ad949940d06494ac2
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-19 17:30:14 +0000

    Updated main.tf

--- X02-IAASPAAS-RocketChat/main.tf
+++ X02-IAASPAAS-RocketChat/main.tf
@@ -78,7 +78,7 @@ resource "azurerm_app_service" "github_app" {
 
 # Load Balancer
 resource "azurerm_lb" "lb" {
-  name                = "my-lb"
+  name                = "sk-lb"
   location            = var.location
   resource_group_name = var.resource_group_name
 
@@ -87,10 +87,11 @@ resource "azurerm_lb" "lb" {
     public_ip_address_id          = azurerm_public_ip.lb_ip.id
     private_ip_address_allocation = "Dynamic"
   }
+}
 
-  backend_address_pool {
-    name = "vmBackendPool"
-  }
+resource "azurerm_lb_backend_address_pool" "vmBackendPool" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "vmBackendPool"
 }
 
 # Public IP Address
