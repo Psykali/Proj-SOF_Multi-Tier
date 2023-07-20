b4e2957a5d601cd3194f55bb20ed127c132ec59d
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-20 09:00:15 +0000

    Updated provider.tf

--- X02-IAASPAAS-RocketChat/provider.tf
+++ X02-IAASPAAS-RocketChat/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.40.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = "sppersotfstates"
+    container_name       = "sakvtfstate"
+    key                  = "rcwikiterraform.tfstate"
+  }
 }
 
 provider "azurerm" {
