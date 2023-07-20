cc3239072c8c902d03b2c638c99b510f13d2a6e1
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-20 08:55:14 +0000

    Updated main.tf

--- OK-XYZ-StorageAcc-KeyVault/main.tf
+++ OK-XYZ-StorageAcc-KeyVault/main.tf
@@ -27,39 +27,3 @@ resource "azurerm_storage_container" "hello_container" {
   name                  = "sakvtfstate"
   storage_account_name  = azurerm_storage_account.sppersotfstates.name
 }
-## Create KeyVault
-resource "azurerm_key_vault" "sppersosecrets" {
-  name = "sppersosecrets"
-  resource_group_name = "PERSO_SIEF"
-  location = "francecentral"
-  sku_name            = "standard"
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-
-tags = {
-    Env = "Prod"
-    Why = "DiplomeP20"
-    CreatedBy = "SK"
-  }
-}
-##
-## Create Secrets
-##resource "azurerm_key_vault_secret" "hello_secret" {
-##  name = "hellosecret"
-##  value = "hello world"
-##  key_vault_id = "${azurerm_key_vault.sppersosecrets.id}"
-##}
-##
-# Terraform OutPut
-# Write the Terraform output to a local file
-resource "local_file" "output" {
-  content  = jsonencode(terraform.workspace)
-  filename = "${path.module}/output.json"
-}
-# Upload the local file to an Azure Blob storage container
-resource "azurerm_storage_blob" "output" {
-  name                   = "output.json"
-  storage_account_name   = azurerm_storage_account.sppersotfstates.name
-  storage_container_name = azurerm_storage_container.hello_container.name
-  type                   = "Block"
-  source                 = local_file.output.filename
-}
