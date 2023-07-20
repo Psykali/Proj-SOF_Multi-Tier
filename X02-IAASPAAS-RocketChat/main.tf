cf2cf69e1084ff5f73a8b6ec52b683e945f6c242
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-20 07:44:19 +0000

    Updated main.tf

--- X02-IAASPAAS-RocketChat/main.tf
+++ X02-IAASPAAS-RocketChat/main.tf
@@ -1,3 +1,10 @@
+###########################################
+## Create Resource Group
+##resource "azurerm_resource_group" "rg" {
+##  name     = var.resource_group_name
+##  location = var.location
+##}
+###########################################
 # App Service Plan
 resource "azurerm_app_service_plan" "app_service_plan" {
   name                = var.app_service_plan_name
