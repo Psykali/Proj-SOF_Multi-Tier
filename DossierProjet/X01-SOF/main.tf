7817d9107217309d3a418a821b7bbb2380c3551b
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-20 16:41:21 +0000

    Added main.tf

---
+++ DossierProjet/X01-SOF/main.tf
@@ -0,0 +1,53 @@
+# Create a virtual machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "h2ogpt-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_B1s"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name                   = "sk-h2ogpt-vm"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+
+provisioner "remote-exec" {
+    inline = [
+      # Update packages
+      "sudo apt-get update",
+
+      # Install dependencies
+      "sudo apt-get install -y python3-pip git",
+
+      # Clone the H2O GPT repository
+      "git clone https://github.com/h2oai/h2ogpt.git",
+
+      # Change to the h2ogpt directory
+      "cd h2ogpt",
+
+      # Install Python packages
+      "pip3 install -r requirements.txt",
+    ]
+    connection {
+        type     = "ssh"
+        user     = var.admin_username
+        password = var.admin_password
+        host     = azurerm_public_ip.public_ip.ip_address
+    }
+}
+}
