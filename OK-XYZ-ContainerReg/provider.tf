575f5e84fe017dbbbfe271a7f6a47ac0a5ee7884
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-20 09:36:00 +0000

    Updated provider.tf

--- OK-XYZ-ContainerReg/provider.tf
+++ OK-XYZ-ContainerReg/provider.tf
@@ -5,7 +5,16 @@ terraform {
       version = "3.40.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "PERSO_SIEF"
+    storage_account_name = "sppersotfstates"
+    container_name       = "sakvtfstate"
+    key                  = "contreg.tfstate"
+  }
 }
+
+data "azurerm_client_config" "current" {}
 
 provider "azurerm" {
   features {}
