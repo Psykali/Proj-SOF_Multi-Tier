266d86710be751b71b6977476bf719ea0c077aaf
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-23 18:51:48 +0000

    Updated WikiJs.sh

--- DossierProjet/X01-SOF/WikiJs.sh
+++ DossierProjet/X01-SOF/WikiJs.sh
@@ -33,15 +33,15 @@ mkdir -p /etc/wiki
 openssl rand -base64 32 > /etc/wiki/.db-secret
 
 # Create internal docker network
-docker network create wikinet
+sudo docker network create wikinet
 
 # Create data volume for PostgreSQL
-docker volume create pgdata
+sudo docker volume create pgdata
 
 # Create the containers
-docker create --name=db -e POSTGRES_DB=wiki -e POSTGRES_USER=wiki -e POSTGRES_PASSWORD_FILE=/etc/wiki/.db-secret -v /etc/wiki/.db-secret:/etc/wiki/.db-secret:ro -v pgdata:/var/lib/postgresql/data --restart=unless-stopped -h db --network=wikinet postgres:11
-docker create --name=wiki -e DB_TYPE=postgres -e DB_HOST=db -e DB_PORT=5432 -e DB_PASS_FILE=/etc/wiki/.db-secret -v /etc/wiki/.db-secret:/etc/wiki/.db-secret:ro -e DB_USER=wiki -e DB_NAME=wiki -e UPGRADE_COMPANION=1 --restart=unless-stopped -h wiki --network=wikinet -p 80:3000 -p 443:3443 ghcr.io/requarks/wiki:2
-docker create --name=wiki-update-companion -v /var/run/docker.sock:/var/run/docker.sock:ro --restart=unless-stopped -h wiki-update-companion --network=wikinet ghcr.io/requarks/wiki-update-companion:latest
+sudo docker create --name=db -e POSTGRES_DB=wiki -e POSTGRES_USER=wiki -e POSTGRES_PASSWORD_FILE=/etc/wiki/.db-secret -v /etc/wiki/.db-secret:/etc/wiki/.db-secret:ro -v pgdata:/var/lib/postgresql/data --restart=unless-stopped -h db --network=wikinet postgres:11
+sudo docker create --name=wiki -e DB_TYPE=postgres -e DB_HOST=db -e DB_PORT=5432 -e DB_PASS_FILE=/etc/wiki/.db-secret -v /etc/wiki/.db-secret:/etc/wiki/.db-secret:ro -e DB_USER=wiki -e DB_NAME=wiki -e UPGRADE_COMPANION=1 --restart=unless-stopped -h wiki --network=wikinet -p 80:3000 -p 443:3443 ghcr.io/requarks/wiki:2
+sudo docker create --name=wiki-update-companion -v /var/run/docker.sock:/var/run/docker.sock:ro --restart=unless-stopped -h wiki-update-companion --network=wikinet ghcr.io/requarks/wiki-update-companion:latest
 
 ################
 ### FireWall ###
@@ -55,9 +55,9 @@ docker create --name=wiki-update-companion -v /var/run/docker.sock:/var/run/dock
 ######################
 ### Containerizing ###
 ######################
-docker start db
-docker start wiki
-docker start wiki-update-companion
+sudo docker start db
+sudo docker start wiki
+sudo docker start wiki-update-companion
 
 ######################
 ### SSL Letencrypt ###
