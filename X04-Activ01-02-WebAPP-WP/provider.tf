03a83df8a828a210758694f7dd90afb30b26a4d4
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-24 15:25:35 +0000

    Updated provider.tf

--- X04-Activ01-02-WebAPP-WP/provider.tf
+++ X04-Activ01-02-WebAPP-WP/provider.tf
@@ -4,18 +4,16 @@ terraform {
       source = "hashicorp/azurerm"
       version = "3.40.0"
     }
-  }
-  required_providers {
-      docker = {
-        source  = "kreuzwerker/docker"
-        version = "~> 3.0"
-      }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
     }
+  }
   backend "azurerm" {
     resource_group_name  = "PERSO_SIEF"
     storage_account_name = "sppersotfstates"
     container_name       = "sakvtfstate"
-    key                  = "lampxwp.tfstate"
+    key                  = "webapps.tfstate"
   }
 }
 
