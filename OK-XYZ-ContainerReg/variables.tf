a7b59d747877ab4a3fb8174f796c5118043c169e
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-24 16:39:19 +0000

    Updated variables.tf

--- OK-XYZ-ContainerReg/variables.tf
+++ OK-XYZ-ContainerReg/variables.tf
@@ -1,19 +1,15 @@
 variable "resource_group_name" {
-  type        = string
   description = "PERSO_SIEF"
 }
 
 variable "location_rg" {
-  type        = string
   description = "francecentral"
 }
 
 variable "location_contreg" {
-  type        = string
   description = "westeurope"
 }
 
 variable "contreg_name" {
-  type        = string
   description = "skP20ContReg"
 }
