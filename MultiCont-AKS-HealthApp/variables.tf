029a353bcf573017e6e813daf81ae40b0059c1aa
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-31 16:04:59 +0000

    Updated variables.tf

--- MultiCont-AKS-HealthApp/variables.tf
+++ MultiCont-AKS-HealthApp/variables.tf
@@ -2,30 +2,36 @@
 ## Ressource Group ##
 #####################
 variable "location" {
+  type        = string
   default = "francecentral"
 }
 
 variable "resource_group_name" {
+  type        = string
   default = "PERSO_SIEF"
 }
 ############
 ## Admin  ##
 ############
 variable "admin_username" {
+  type        = string
   default = "PsykProjsP20"
 }
 
 variable "admin_password" {
+  type        = string
   default = "x*axbUNUeBJE^Jpwc%4*h"
 }
 #########################
 ## Container Registrey ##
 #########################
 variable "contreg_name" {
+  type        = string
   default = "psykprojs-acr"
 }
 
 variable "location_contreg" {
+  type        = string
   description = "westeurope"
 }
 
@@ -33,17 +39,21 @@ variable "location_contreg" {
 ## Azure AKS ##
 ###############
 variable "kubernetes_cluster_name" {
+  type        = string
   default = "psykprojs-aks"
 }
 
 variable "dns_prefix" {
+  type        = string
   description = "psykprojs-aks"
 }
 
 variable "node_pool_name" {
+  type        = string
   description = "psykprojs-agentpool"
 }
 
 variable "namespace_name" {
+  type        = string
   description = "healthappli"
 }
