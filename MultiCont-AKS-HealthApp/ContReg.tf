881b7bc1f6dba7f00f609d58b79d99caa5b7ebf8
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-31 14:31:40 +0000

    Added ContReg.tf

---
+++ MultiCont-AKS-HealthApp/ContReg.tf
@@ -0,0 +1,11 @@
+resource "azurerm_container_registry" "psykprojs" {
+  name                = "psykprojs-acr"
+  resource_group_name = var.resource_group_name
+  location            = var.location_contreg
+  sku                 = "Standard"
+  admin_enabled       = true
+  tags = {
+    displayName          = "Container Registry"
+    "container.registry" = "psykprojs-acr"
+  }
+}
