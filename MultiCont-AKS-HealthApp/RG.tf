919591abe63983d7cf8f3f34998a35efe3557829
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-07-31 15:17:52 +0000

    Updated RG.tf

--- MultiCont-AKS-HealthApp/RG.tf
+++ MultiCont-AKS-HealthApp/RG.tf
@@ -1,5 +1,7 @@
-## Create ressource Group
-resource "azurerm_resource_group" "psykprojs" {
-  name     = var.resource_group_name
-  location = var.location
-}
+############################
+## Create ressource Group ##
+############################
+#resource "azurerm_resource_group" "psykprojs" {
+#  name     = var.resource_group_name
+#  location = var.location
+#}
