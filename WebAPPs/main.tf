b052099634939f6e566830bd31c3f9fec31264ab
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-01 10:36:30 +0000

    Updated main.tf

--- WebAPPs/main.tf
+++ WebAPPs/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_app_service" "example" {
   app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
-    dotnet_framework_version = "v4.8"
+    dotnet_framework_version = "v4.0"
   }
 
   app_settings = {
