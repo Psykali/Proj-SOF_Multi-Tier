4c9b0725e63d3c7e315115df2dcb054b632e6c10
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-01 13:39:46 +0000

    Updated variables.tf

--- VMwithDocker/variables.tf
+++ VMwithDocker/variables.tf
@@ -1,47 +1,59 @@
+#####################
+## Ressource Group ##
+#####################
 variable "location" {
-  default = "westeurope"
+  type        = string
+  default = "francecentral"
 }
 
 variable "resource_group_name" {
+  type        = string
   default = "PERSO_SIEF"
 }
-
+############
+## Admin  ##
+############
 variable "admin_username" {
-  default = "SkLoginDipP20"
+  type        = string
+  default = "PsykProjsP20"
 }
 
 variable "admin_password" {
-  default = "P@ssw0rd123P@ssw0rd123"
+  type        = string
+  default = "x*axbUNUeBJE^Jpwc%4*h"
+}
+#########################
+## Container Registrey ##
+#########################
+variable "contreg_name" {
+  type        = string
+  default = "psykprojsacr"
 }
 
-variable "sql_server_name" {
-  default = "skwpp20-sqlserver"
+variable "location_contreg" {
+  type        = string
+  default = "westeurope"
 }
 
-variable "sql_server_fqdn" {
-  default = "skwpp20-sqlserver.database.windows.net:1433"
+###############
+## Azure AKS ##
+###############
+variable "kubernetes_cluster_name" {
+  type        = string
+  default = "psykprojsaks"
 }
 
-variable "sql_database_name" {
-  default = "skwp-sqldb"
+variable "dns_prefix" {
+  type        = string
+  default = "psykprojsaks"
 }
 
-variable "container_name" {
-  default = "skdwpp20cont"
+variable "node_pool_name" {
+  type        = string
+  default = "psykprojspl"
 }
 
-variable "image_name" {
-  default = "skp20contreg.azurecr.io/wordpress:latest"
-}
-
-##variable "scope_map_token_name" {
-##  default = "docker login -u Admin -p Zxq1EiAjiDl3BEDNbRXBzrjN7szlmJ+DLtdFe63Vn4+ACRCPwHZC skp20contreg.azurecr.io"
-##}
-
-variable "scope_map_token_name" {
-  default = "Admin"
-}
-
-variable "scope_map_token_password" {
-  default = "Zxq1EiAjiDl3BEDNbRXBzrjN7szlmJ+DLtdFe63Vn4+ACRCPwHZC"
+variable "namespace_name" {
+  type        = string
+  default = "healthappli"
 }
