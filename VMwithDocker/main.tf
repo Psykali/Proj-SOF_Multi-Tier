3b7387fba2339c6a7f70d4b56f5042092e9da11b
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-01 14:06:58 +0000

    Updated main.tf

--- VMwithDocker/main.tf
+++ VMwithDocker/main.tf
@@ -1,73 +1,63 @@
-###########################################################
-### Create the resource group
+data "azurerm_client_config" "current" {}
+
+locals {
+  common_tags = {
+    CreatedBy = "SK"
+    Env       = "Prod"
+    Why       = "DipP20"
+  }
+}
+###########################################
+## Create Resource Group
 ##resource "azurerm_resource_group" "rg" {
 ##  name     = var.resource_group_name
 ##  location = var.location
 ##}
-###########################################################
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet"
+###########################################
+# Create VM
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.ubuntu-vm
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
+  size                = "Standard_B1s"
+  disable_password_authentication= false
+
+network_interface_ids= [
+    azurerm_network_interface.default.id,
+]
+
+os_disk {
+    caching              = "ReadWrite"
+    storage_account_type= "Standard_LRS"
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+source_image_reference {
+    publisher= "Canonical"
+    offer    = "UbuntuServer"
+    sku      = "18.04-LTS"
+    version= "latest"
 }
 
-resource "azurerm_public_ip" "public_ip" {
-  name                = "publicip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
+  admin_username= var.admin_username
+  admin_password= var.admin_password
+
+  tags = local.common_tags
 }
+#######################################################################
+#######################################################################
+## Bash Scripting
+# Deploy LAMP Server Ports 80, 443, 8050, 3306
+resource "null_resource" "install_wordpress" {
+  depends_on = [
+    azurerm_linux_virtual_machine.vm,
+  ]
 
-resource "azurerm_network_interface" "nic" {
-  name                      = "nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "ipconfig"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  connection {
+    type     = "ssh"
+    user     = var.admin_username
+    password = var.admin_password
+    host     = azurerm_linux_virtual_machine.vm.public_ip_address
   }
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "my-vm"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_DS2_v2"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
-    version   = "latest"
-  }
-
-  os_disk {
-    name              = "osdisk"
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  admin_username = "adminuser"
-  admin_password = "P@ssw0rd1234!"
 
   provisioner "remote-exec" {
     inline = [
