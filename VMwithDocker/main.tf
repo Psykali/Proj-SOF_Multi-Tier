3749883068c7fab3e66350f887a02e0053cfd831
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-01 15:14:35 +0000

    Updated main.tf

--- VMwithDocker/main.tf
+++ VMwithDocker/main.tf
@@ -58,29 +58,22 @@ resource "null_resource" "install_wordpress" {
   }
 
   provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update && sudo apt-get -y upgrade",
-      "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-      "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-      "sudo apt-get update",
-      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-      "sudo curl -L 'https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose",
-      "sudo chmod +x /usr/local/bin/docker-compose",
-    "sudo apt-key adv --fetch-keys 'https://dev.mysql.com/doc/mysql-apt-repo-quick-guide/en/Quick_Install_Guide.gpg'",
-    "sudo add-apt-repository 'deb [arch=amd64] https://repo.mysql.com/apt/ubuntu $(lsb_release -cs) mysql-8.0'",
+  inline = [
     "sudo apt-get update",
-    "sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password P@ssw0rd1234!'",
-    "sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password P@ssw0rd1234!'",
-    "sudo apt-get install -y mysql-server",
-    "sudo systemctl start mysql",
-    "sudo systemctl enable mysql",
-    "sudo apt-get install -y python3-pip",  # Install pip for Python 3
-    "sudo ln -s /usr/bin/python3 /usr/bin/python",  # Create a symbolic link for python command
-    "sudo -H pip3 install --upgrade pip",  # Upgrade pip to the latest version
+    "sudo apt-get upgrade -y",
+    "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
+    "sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'",
+    "sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mirror.zol.co.zw/mariadb/repo/10.6/ubuntu $(lsb_release -cs) main'",
+    "sudo apt-get update",
+    "sudo debconf-set-selections <<< 'mariadb-server-10.6 mysql-server/root_password password P@ssw0rd1234!'",
+    "sudo debconf-set-selections <<< 'mariadb-server-10.6 mysql-server/root_password_again password P@ssw0rd1234!'",
+    "sudo apt-get install -y mariadb-server",
+    "sudo systemctl start mariadb",
+    "sudo systemctl enable mariadb",
+    "sudo apt-get install -y python3-pip",
+    "sudo ln -s /usr/bin/python3 /usr/bin/python",
+    "sudo -H pip3 install --upgrade pip",
     "sudo -H pip3 install streamlit langchain openai wikipedia chromadb tiktoken",
   ]
-  }
+}
 }
