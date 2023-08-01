277a21a5a771c1d35653bfe3be686d562cd0bdd5
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-01 13:32:42 +0000

    Updated provider.tf

--- VMwithDocker/provider.tf
+++ VMwithDocker/provider.tf
@@ -1,3 +1,6 @@
+###############
+## Providers ##
+###############
 terraform {
   required_providers {
     azurerm = {
@@ -5,7 +8,16 @@ terraform {
       version = "3.40.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "PERSO_SIEF"
+    storage_account_name = "sppersotfstates"
+    container_name       = "sakvtfstate"
+    key                  = "vmdocker.tfstate"
+  }
 }
+
+data "azurerm_client_config" "current" {}
 
 provider "azurerm" {
   features {}
