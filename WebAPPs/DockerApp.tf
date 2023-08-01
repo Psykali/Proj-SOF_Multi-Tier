e331b0cb8a5afcf978330f719c52323087481824
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-01 12:29:32 +0000

    Updated DockerApp.tf

--- WebAPPs/DockerApp.tf
+++ WebAPPs/DockerApp.tf
@@ -19,3 +19,47 @@ resource "azurerm_app_service" "example" {
     environment = "Production"
   }
 }
+
+resource "azurerm_app_service" "example" {
+  name                = "psykprojsofdck"
+  location            = "France Central"
+  resource_group_name = "PERSO_SIEF"
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    always_on = true
+
+    # Set the container settings for the Docker image deployment
+    linux_fx_version = "DOCKER|psykali/stackoverp20kcab:latest"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "psykprojwikidck"
+  location            = "France Central"
+  resource_group_name = "PERSO_SIEF"
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    always_on = true
+
+    # Set the container settings for the Docker image deployment
+    linux_fx_version = "DOCKER|linuxserver/wikijs:latest"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
