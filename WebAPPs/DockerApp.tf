c4824c2ad8fee424adc608b3c5ea6b93e00e2ea6
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-01 12:25:57 +0000

    Added DockerApp.tf

---
+++ WebAPPs/DockerApp.tf
@@ -0,0 +1,21 @@
+resource "azurerm_app_service" "example" {
+  name                = "psykprojwbdckr"
+  location            = "France Central"
+  resource_group_name = "PERSO_SIEF"
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    always_on = true
+
+    # Set the container settings for the Docker image deployment
+    linux_fx_version = "DOCKER|climatemind/webapp:production"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
