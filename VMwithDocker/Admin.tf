4fc2b861ac62cf81b69adbb6e02de68551c6db63
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-03 10:14:08 +0000

    Updated Admin.tf

--- VMwithDocker/Admin.tf
+++ VMwithDocker/Admin.tf
@@ -80,8 +80,12 @@ resource "null_resource" "install_packages" {
 
 provisioner "remote-exec" {
   inline = [
-    "sudo apt-get update",
-    "sudo apt-get upgrade -y",
+        "sudo apt-get update && sudo apt-get -y upgrade", 
+        "sudo apt update && sudo apt -y upgrade",
+        "sudo apt -y install docker.io",
+        "sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose",
+        "sudo chmod +x /usr/local/bin/docker-compose",
   ]
 }
 }
+
