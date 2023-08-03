70098ad9d35ca926f6a2e293c1d8ba8a75c564f8
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-03 09:39:06 +0000

    Updated GitLab-VM.tf

--- VMwithDocker/GitLab-VM.tf
+++ VMwithDocker/GitLab-VM.tf
@@ -1,1 +1,82 @@
-##
+#######################
+## Create Git lab VM ##
+#######################
+resource "azurerm_linux_virtual_machine" "gitlab_vm" {
+  name                = var.gitlab_vm
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = "Standard_B2ms"
+  disable_password_authentication= false
+
+network_interface_ids= [
+    azurerm_network_interface.gitlab_nic.id,
+]
+
+os_disk {
+    caching              = "ReadWrite"
+    storage_account_type= "Standard_LRS"
+}
+
+source_image_reference {
+    publisher= "Canonical"
+    offer    = "UbuntuServer"
+    sku      = "18.04-LTS"
+    version= "latest"
+}
+
+  admin_username= var.admin_username
+  admin_password= var.admin_password
+
+  tags = local.common_tags
+}
+##############################
+## Create Network Interface ##
+##############################
+resource "azurerm_network_interface" "gitlab_nic" {
+  name = var.gitlab_nic
+  location = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = var.gitlab_ip
+    subnet_id                     = azurerm_subnet.default.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.gitlab_pip.id
+  }
+  tags = local.common_tags
+}
+################################
+## Create a public IP address ##
+################################
+resource "azurerm_public_ip" "gitlab_pip" {
+  name                = var.gitlab_pip
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+  domain_name_label   = var.gitlab_vm
+  tags = local.common_tags
+}
+#######################################################################
+####################
+## Bash Scripting ##
+####################
+# Deploy Git Server
+resource "null_resource" "install_packages" {
+  depends_on = [
+    azurerm_linux_virtual_machine.gitlab_vm,
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = var.admin_username
+    password = var.admin_password
+    host     = azurerm_linux_virtual_machine.gitlab_vm.public_ip_address
+  }
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo apt-get update",
+    "sudo apt-get upgrade -y",
+  ]
+}
+}
