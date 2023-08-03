08851f323610e54f97944250b30dbee205b2886e
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-03 08:48:41 +0000

    Updated variables.tf

--- VMwithDocker/variables.tf
+++ VMwithDocker/variables.tf
@@ -2,55 +2,109 @@
 ## Ressource Group ##
 #####################
 variable "location" {
+  description = "Value of the Ressource Group Locations"
+  type        = string
   default = "francecentral"
 }
 
 variable "resource_group_name" {
+  description = "Value of the Ressource Group name"
+  type        = string
   default = "PERSO_SIEF"
 }
 ############
 ## Admin  ##
 ############
 variable "admin_username" {
-  default = "SkLoginDipP20"
+  description = "Value of the Admin login"
+  type        = string
 }
 
 variable "admin_password" {
-  default = "P@ssw0rd123P@ssw0rd123"
+  description = "Value of the Admin Pass"
+  type        = string
 }
 ########
 ## VM ##
 ########
-variable "ubuntu-vm" {
-  default = "skwpp20lamp"
+variable "admin_vm" {
+  description = "Value of the Ressource Group Locations"
+  type        = string
+  default = "adminpsykprojs"
+}
+
+variable "web_vm" {
+  description = "Value of the Ressource Group Locations"
+  type        = string
+  default = "webpsykprojs"
+}
+
+variable "wiki_vm" {
+  description = "Value of the Ressource Group Locations"
+  type        = string
+  default = "wikipsykprojs"
+}
+
+variable "gitlab_vm" {
+  description = "Value of the Ressource Group Locations"
+  type        = string
+  default = "gitpsykprojs"
+}
+
+variable "clearenceai_vm" {
+  description = "Value of the Ressource Group Locations"
+  type        = string
+  default = "clearenceaipsykprojs"
 }
 ################
 ## Networking ##
 ################
-variable "subnet" {
-  default = "skwpp20subnet"
+variable "subnet_name" {
+  description = "Value of the Subnet name"
+  type        = string
+  default = "subnetpsyckprojs"
 }
 
 variable "virtual_network_name" {
-  default = "skwpp20vnet"
+  description = "Value of the Vnet name"
+  type        = string
+  default = "vnetpsyckprojs"
 }
 
-variable "virtual_network" {
-  default = "skwpp20net"
-}
-
-variable "network_interface" {
-  default = "skwpp20nic"
+variable "network_interface_name" {
+  description = "Value of the NIC name"
+  type        = string
+  default = "nicpsyckprojs"
 }
 
 variable "network_security_group_name" {
-  default = "skwpp20nsg"
+  description = "Value of the NSG name"
+  type        = string
+  default = "nsgpsyckprojs"
 }
 
-variable "ubuntu-ipconfig" {
-  default = "skwpp20ip"
+variable "public_ip_name" {
+  description = "Value of the Public IP name"
+  type        = string
+  default = "pippsyckprojs"
 }
 
-variable "ubuntu-pip" {
-  default = "skwpp20pip"
+variable "load_balancer_name" {
+  description = "Value of the Load Balancer name"
+  type        = string
+  default = "lbpsyckprojs"
+}
+#####################
+## Storage Account ##
+#####################
+variable "storage_account_name" {
+  description = "Value of the Storage Account name"
+  type        = string
+  default = "sapsyckprojs"
+}
+
+variable "container_name" {
+  description = "Value of the Vnet name"
+  type        = string
+  default = "wppsyckprojs"
 }
