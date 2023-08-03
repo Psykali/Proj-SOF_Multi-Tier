ce9f43648f076efabfa88c410fd696549d9c66aa
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-03 14:15:09 +0000

    Updated variables.tf

--- VMwithDocker/variables.tf
+++ VMwithDocker/variables.tf
@@ -18,11 +18,13 @@ variable "resource_group_name" {
 variable "admin_username" {
   description = "Value of the Admin login"
   type        = string
+  default = "SkLoginDipP20"
 }
 
 variable "admin_password" {
   description = "Value of the Admin Pass"
   type        = string
+  default = "V83phJJDRExKW3kmhLCm4"
 }
 ########
 ## VM ##
