d191ee4c67aa5d75662d7f01ceb85a697bbd2634
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-03 13:20:16 +0000

    Updated WebServ-VM.tf

--- VMwithDocker/WebServ-VM.tf
+++ VMwithDocker/WebServ-VM.tf
@@ -71,7 +71,7 @@ resource "azurerm_mysql_database" "web_db" {
 ## Bash Scripting ##
 ####################
 # Deploy Git Server
-resource "null_resource" "install_packages" {
+resource "null_resource" "install_packages_web_server" {
    depends_on = [
     azurerm_linux_virtual_machine.web_vm,
     azurerm_mysql_database.web_db,
