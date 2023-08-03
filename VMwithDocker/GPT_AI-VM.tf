679eaa10f3e87ebd38749d1f9072bf53a7e4808c
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-03 13:19:14 +0000

    Updated GPT_AI-VM.tf

--- VMwithDocker/GPT_AI-VM.tf
+++ VMwithDocker/GPT_AI-VM.tf
@@ -71,7 +71,7 @@ resource "azurerm_mysql_database" "clearenceai_db" {
 ## Bash Scripting ##
 ####################
 # Deploy ClearenceAI Server
-resource "null_resource" "install_packages" {
+resource "null_resource" "install_packages_gpt" {
   depends_on = [
     azurerm_linux_virtual_machine.clearenceai_vm,
     azurerm_mysql_database.clearenceai_db,
