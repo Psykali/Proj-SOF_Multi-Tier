c430358e4daa963c75f50c1b89d7552620b48dc9
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-04 08:56:18 +0000

    Updated ChatEticketing.tf

--- Infra_IAASSAAS/ChatEticketing.tf
+++ Infra_IAASSAAS/ChatEticketing.tf
@@ -29,6 +29,41 @@ source_image_reference {
 
   tags = local.common_tags
 }
+## Metrics and Alerts
+resource "azurerm_monitor_metric_alert" "chattickting_vm" {
+  name                = "chatticktingvm-CPU"
+  resource_group_name = var.resource_group_name
+  scopes              = azurerm_linux_virtual_machine.chattickting_vm.id
+  description         = "Action will be triggered when CPU usage exceeds 80% for 5 minutes."
+
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Percentage CPU"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 80
+  }
+
+  window_size        = "PT15M"
+  frequency          = "PT5M"
+}
+resource "azurerm_monitor_metric_alert" "chatticktingvm" {
+  name                = "chattickting-MeM"
+  resource_group_name = var.resource_group_name
+  scopes              = azurerm_linux_virtual_machine.chattickting_vm.id
+  description         = "Action will be triggered when available memory falls below 20% for 5 minutes."
+
+  criteria {
+    metric_namespace = "Microsoft.Compute/virtualMachines"
+    metric_name      = "Available Memory Bytes"
+    aggregation      = "Average"
+    operator         = "LessThan"
+    threshold        = 0.2
+  }
+
+  window_size        = "PT15M"
+  frequency          = "PT5M"
+}
 ##############################
 ## Create Network Interface ##
 ##############################
