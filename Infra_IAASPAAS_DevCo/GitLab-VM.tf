f002e03436e320bce737bcdb8cddff96fb619910
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-04 09:37:03 +0000

    Updated GitLab-VM.tf

--- Infra_IAASPAAS_DevCo/GitLab-VM.tf
+++ Infra_IAASPAAS_DevCo/GitLab-VM.tf
@@ -33,7 +33,7 @@ source_image_reference {
 resource "azurerm_monitor_metric_alert" "gitlab_vm" {
   name                = "gitlab-CPU"
   resource_group_name = var.resource_group_name
-  scopes              = azurerm_linux_virtual_machine.gitlab_vm.id
+  scopes              = [azurerm_linux_virtual_machine.gitlab_vm.id]
   description         = "Action will be triggered when CPU usage exceeds 80% for 5 minutes."
 
   criteria {
@@ -50,7 +50,7 @@ resource "azurerm_monitor_metric_alert" "gitlab_vm" {
 resource "azurerm_monitor_metric_alert" "gitlabvm" {
   name                = "gitlab-MeM"
   resource_group_name = var.resource_group_name
-  scopes              = azurerm_linux_virtual_machine.gitlab_vm.id
+  scopes              = [azurerm_linux_virtual_machine.gitlab_vm.id]
   description         = "Action will be triggered when available memory falls below 20% for 5 minutes."
 
   criteria {
