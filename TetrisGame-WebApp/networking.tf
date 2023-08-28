77bf3e8682c9f718c45c7c7fe6134efb080f1196
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-28 15:50:03 +0000

    Updated networking.tf

--- TetrisGame-WebApp/networking.tf
+++ TetrisGame-WebApp/networking.tf
@@ -27,35 +27,78 @@ resource "azurerm_app_service" "example" {
   }
 }
 
-resource "azurerm_traffic_manager_profile" "example" {
-  name                   = "example-trafficmanagerprofile"
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  traffic_routing_method = "Weighted"
-
-  dns_config {
-    relative_name = "example-trafficmanagerprofile"
-    ttl           = 100
-  }
-
-  monitor_config {
-    protocol                     = "http"
-    port                         = 80
-    path                         = "/"
-    interval_in_seconds          = 30
-    timeout_in_seconds           = 10
-    tolerated_number_of_failures = 3
-    custom_header_host           = ""
-    expected_status_code_ranges {
-      min   = 200
-      max   = 299
-    }
-  }
+  address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_traffic_manager_endpoint" "example" {
-  count              = 2
-  name               = "example-endpoint${count.index + 1}"
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-pip"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  profile_name       = azurerm_traffic_manager_profile.example.name
-  target_resource_id = azurerm_app_service.example[count.index].id
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_application_gateway" "example" {
+  name                = "example-appgateway"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  sku {
+    name     = "Standard_Small"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "example-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.example.id
+  }
+
+  frontend_port {
+    name = "example-frontend-port"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "example-frontend-ip-configuration"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+
+  backend_address_pool {
+    name         = "example-backend-address-pool"
+    ip_addresses = [azurerm_app_service.example[0].default_site_hostname, azurerm_app_service.example[1].default_site_hostname]
+  }
+
+  backend_http_settings {
+    name                  = "example-backend-http-settings"
+    cookie_based_affinity = "Disabled"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = "example-http-listener"
+    frontend_ip_configuration_name = "example-frontend-ip-configuration"
+    frontend_port_name             = "example-frontend-port"
+    protocol                       <EUGPSCoordinates>Http"
+}
+
+resource "azurerm_application_gateway_request_routing_rule" "example" {
+    name                       <EUGPSCoordinates>example-request-routing-rule"
+    rule_type                  <EUGPSCoordinates>Basic"
+    http_listener_name         <EUGPSCoordinates>example-http-listener"
+    backend_address_pool_name   <EUGPSCoordinates>example-backend-address-pool"
+    backend_http_settings_name <EUGPSCoordinates>example-backend-http-settings"
 }
