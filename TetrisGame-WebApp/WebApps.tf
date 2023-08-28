d444e0004d7748fe74e488ceadcfad7771374a0c
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-28 14:20:18 +0000

    Updated WebApps.tf

--- TetrisGame-WebApp/WebApps.tf
+++ TetrisGame-WebApp/WebApps.tf
@@ -15,205 +15,60 @@ locals {
 #  name     = "PERSO_SIEF"
 #  location = "France Central"
 #}
-###########################
-## Create the SQL Server ##
-###########################
-resource "azurerm_sql_server" "sql_backend_pool" {
-  name                         = "sqlserv"
-  resource_group_name          = var.resource_group_name
-  location                     = var.location
-  version                      = "12.0"
-  administrator_login          = var.admin_username
-  administrator_login_password = var.admin_password
-}
-
-##############################
-## Create the SQL databases ##
-##############################
-resource "azurerm_sql_database" "sql_backend_pool" {
-  count               = 3
-  name                = "sqldb-${count.index}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  server_name         = azurerm_sql_server.sql_backend_pool.name
-}
-
-######################################
-## Create the load balancer for SQL ##
-######################################
-resource "azurerm_lb" "sqldbbkndlb" {
-  name                = "sqldbbkndlb"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = "Standard"
-
-  frontend_ip_configuration {
-    name                          = "PrivateIPAddress"
-    private_ip_address_allocation = "Dynamic"
-    private_ip_address_version    = "IPv4"
-    subnet_id                     = azurerm_subnet.example.id # Update this to reference an existing subnet resource
-  }
-}
-
-####################################################################
-## Add the SQL databases to the backend pool of the load balancer ##
-####################################################################
-resource "azurerm_lb_backend_address_pool_address" "sql_backend_pool" {
-  count                   = length(azurerm_sql_database.sql_backend_pool)
-  backend_address_pool_id = azurerm_lb.sqldbbkndlb.backend_address_pool[0].id # Add this argument
-  name                    = "sql_backend_pool-${count.index}"
-  ip_address              = azurerm_sql_database.sql_backend_pool[count.index].id # Update this argument
-}
 #####################
 ## Create App Plan ##
 #####################
 resource "azurerm_app_service_plan" "example" {
-  name                = "app-service-plan"
-  location            = "France Central"
-  resource_group_name = "PERSO_SIEF"
-  kind                = "Linux"
-  reserved            = true
+  name                = "tetris-asp"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   sku {
-    tier = "Basic"
-    size = "B1"
+    tier = "Standard"
+    size = "S1"
   }
 }
 ##################################
 ## Create Web App for WordPress ##
 ##################################
-variable "app_names" {
-  type = list(string)
-  default = ["1stwppsyckprjst", "2ndwppsyckprjs", "3rdwppsyckprjs"]
-}
-resource "azurerm_app_service" "wordpress" {
-  count               = length(var.app_names)
-  name                = var.app_names[count.index]
+resource "azurerm_app_service" "example" {
+  count               = 3
+  name                = "tetris-app${count.index}"
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
-    always_on = true
-    linux_fx_version = "DOCKER|wordpress:latest"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = local.common_tags
-}
-
-resource "azurerm_app_service_slot" "example" {
-  app_service_name       = azurerm_app_service.wordpress[0].name
-  location               = azurerm_app_service.wordpress[0].location
-  resource_group_name    = azurerm_app_service.wordpress[0].resource_group_name
-  app_service_plan_id    = azurerm_app_service_plan.example.id
-  name                   = "staging"
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLAzure"
-    value = "Server=tcp:${azurerm_lb.sqldbbkndlb.private_ip_address},1433;Initial Catalog=sqldb-0;User ID=${var.admin_username};Password=${var.admin_password};"
+    linux_fx_version = "DOCKER|<your_docker_image>"
   }
 }
-################################################
-## Create Public IP address for Load Balancer ##
-################################################
-resource "azurerm_public_ip" "lb_pip" {
-  name                = "lb-public-ip"
-  location            = var.location
+
+resource "azurerm_traffic_manager_profile" "example" {
+  name                = "tetris-tmp"
   resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = local.common_tags
+  traffic_routing_method = "Weighted"
+
+  dns_config {
+    relative_name = "example-dns"
+    ttl           = 100
+  }
+
+  monitor_config {
+    protocol                     = "http"
+    port                         = 80
+    path                         = "/"
+    interval_in_seconds          = 30
+    timeout_in_seconds           = 9
+    tolerated_number_of_failures = 3
+  }
 }
-##################################
-## Create Load Balancer WebApps ##
-##################################
-resource "azurerm_lb" "lb" {
-  name                = "webapp-lb"
-  location            = var.location
+
+resource "azurerm_traffic_manager_endpoint" "example" {
+  count               = length(azurerm_app_service.example)
+  name                = "example-endpoint${count.index}"
   resource_group_name = var.resource_group_name
-  sku                 = "Standard"
-
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.lb_pip.id
-  }
-  tags = local.common_tags
+  profile_name        = azurerm_traffic_manager_profile.example.name
+  target_resource_id   = element(azurerm_app_service.example.*.id, count.index)
+  type                 = "azureEndpoints"
 }
-#######################################
-## backend pool of the load balancer ##
-#######################################
-resource "azurerm_lb_backend_address_pool" "example" {
-  loadbalancer_id = azurerm_lb.lb.id
-  name            = "BackendPool"
-}
-###############################################################
-## Add the Web Apps to the backend pool of the load balancer ##
-###############################################################
-resource "azurerm_lb_backend_address_pool_address" "example" {
-  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
-  name                    = "wordpressbkndpl"
-  ip_address              = azurerm_linux_virtual_machine.example.private_ip_address # Update this argument
-}
-#######################
-## Create Front Door ##
-#######################
-resource "azurerm_frontdoor" "frontdoor" {
-  name                = "webapp-frontdoor"
-  location            = var.location
-  resource_group_name = var.resource_group_name
 
-  routing_rule {
-    name               = "webapp-routing-rule"
-    frontend_endpoints = [azurerm_frontdoor_frontend_endpoint.frontend.id]
-    accepted_protocols = ["Http", "Https"]
-    patterns_to_match  = ["/*"]
-    forwarding_configuration {
-      backend_pool_name = azurerm_lb_backend_address_pool.backend_pool.name
-      backend_protocol  = "Http"
-      backend_host_header = azurerm_app_service.wordpress_primary.default_site_hostname
-    }
-  }
-
-  frontend_endpoint {
-    name                 = "webapp-frontend"
-    host_name            = azurerm_public_ip.lb_pip.fqdn
-    session_affinity_enabled = true
-    session_affinity_ttl_seconds = 300
-  }
-
-  tags = local.common_tags
-}
-#######################
-## Create Front Door ##
-#######################
-resource "azurerm_frontdoor" "frontdoor" {
-  name                = "webapp-frontdoor"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  routing_rule {
-    name               = "webapp-routing-rule"
-    frontend_endpoints = [azurerm_frontdoor.frontdoor.frontend_endpoint[0].id] # Update this argument
-    accepted_protocols = ["Http", "Https"]
-    patterns_to_match  = ["/*"]
-    forwarding_configuration {
-      backend_pool_name = azurerm_lb_backend_address_pool.backend_pool.name
-      backend_protocol  = "Http"
-      backend_host_header = azurerm_app_service.wordpress.default_site_hostname
-    }
-  }
-
-  frontend_endpoint {
-    name                 = "webapp-frontend"
-    host_name            = azurerm_public_ip.lb_pip.fqdn
-    session_affinity_enabled = true
-    session_affinity_ttl_seconds = 300
-  }
-
-  tags = local.common_tags
-}
