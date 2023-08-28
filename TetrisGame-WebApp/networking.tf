eb200ddf3b54542f3f4c8a3f94211ab474b0a7c5
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-28 15:32:01 +0000

    Updated networking.tf

--- TetrisGame-WebApp/networking.tf
+++ TetrisGame-WebApp/networking.tf
@@ -1,118 +1,67 @@
-resource "azurerm_virtual_network" "example" {
-  name                = "tetris-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  subnet {
-    name           = "example-subnet"
-    address_prefix = "10.0.1.0/24"
-  }
-}
-
-resource "azurerm_subnet_network_security_group_association" "example" {
-  subnet_id                 = azurerm_virtual_network.example.subnet[0].id
-  network_security_group_id = azurerm_network_security_group.example.id
-}
-
-resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  security_rule {
-    name                       = "http-rule"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "https-rule"
-    priority                   = 101
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "example-pip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_lb_backend_address_pool" "example" {
-  loadbalancer_id = azurerm_lb.example.id
-  name            = "example-backend-pool"
-}
-
-resource "azurerm_lb_probe" "example" {
-  loadbalancer_id = azurerm_lb.example.id
-  name            = "example-probe"
-  protocol        = "Http"
-  request_path    = "/"
-  port            = 80
-  interval_seconds          = 30
-  number_of_probes          = 2
-  load_balancing_rule_ids   = [azurerm_lb_rule.example.http.id, azurerm_lb_rule.example.https.id]
-  protocol_match_value      = "200-399"
-  protocol_match_criteria   = "StatusCodes"
-}
-
-resource "azurerm_lb_rule" "example" {
-  count                     = 2
-  name                      = count.index == 0 ? "http" : "https"
-  protocol                  = count.index == 0 ? "Tcp" : "Https"
-  frontend_port             = count.index == 0 ? 80 : 443
-  backend_port              = 80
-  frontend_ip_configuration = azurerm_lb.example.frontend_ip_configuration[0].id
-  backend_address_pool_id   = azurerm_lb.example.backend_address_pool.id
-  probe_id                  = azurerm_lb.example.probe.id
-}
-
 resource "azurerm_app_service_plan" "example" {
-  name                = "example-asp"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Basic"
+    size = "B1"
   }
 }
 
 resource "azurerm_app_service" "example" {
-  count                     = 3
-  name                      = "example-webapp-${count.index}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  app_service_plan_id       = azurerm_app_service_plan.example.id
-  enabled                   = true
-  https_only                = true
-  site_config {
-    linux_fx_version        = "DOCKER|skP20ContReg.azurecr.io/tetrisgameapp:602"
-  }
+  count               = 2
+  name                = "example-app-service${count.index + 1}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
-    always_on = true
-    http2_enabled = true
-    websockets_enabled = true
+    linux_fx_version = "DOCKER|skP20ContReg.azurecr.io/tetrisgameapp"
   }
 
-  identity {
-    type = "SystemAssigned"
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+  }
+}
+
+resource "azurerm_traffic_manager_profile" "example" {
+  name                   = "example-trafficmanagerprofile"
+  resource_group_name    = azurerm_resource_group.example.name
+  traffic_routing_method = "Weighted"
+
+  dns_config {
+    relative_name = "example-trafficmanagerprofile"
+    ttl           = 100
   }
 
+  monitor_config {
+    protocol                     = "http"
+    port                         = 80
+    path                         = "/"
+    interval_in_seconds          = 30
+    timeout_in_seconds           = 10
+    tolerated_number_of_failures = 3
+    custom_header_host           = ""
+    expected_status_code_ranges {
+      min   = 200
+      max   = 299
+    }
+  }
+}
+
+resource "azurerm_traffic_manager_endpoint" "example1" {
+  name                = "example-endpoint1"
+  resource_group_name = azurerm_resource_group.example.name
+  profile_name        = azurerm_traffic_manager_profile.example.name
+  target_resource_id   <EUGPSCoordinates>azurerm_app_service.example1.id
+}
+
+resource "azurerm_traffic_manager_endpoint" "example2" {
+  name                = "example-endpoint2"
+  resource_group_name = azurerm_resource_group.example.name
+  profile_name        = azurerm_traffic_manager_profile.example.name
+   <EUGPSCoordinates>azurerm_app_service.example2.id
 }
