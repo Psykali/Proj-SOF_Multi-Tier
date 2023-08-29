71efb013d8c03f6d8b0796ef0d76d969b4b6fc89
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-29 09:35:49 +0000

    Updated networking.tf

--- TetrisGame-WebApp/networking.tf
+++ TetrisGame-WebApp/networking.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
+  name                = "skttrsp20-appserviceplan"
   location            = var.location
   resource_group_name = var.resource_group_name
   kind                = "Linux"
@@ -13,7 +13,7 @@ resource "azurerm_app_service_plan" "example" {
 
 resource "azurerm_app_service" "example" {
   count               = 2
-  name                = "example-app-service${count.index + 1}"
+  name                = "skttrsp20${count.index + 1}"
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = azurerm_app_service_plan.example.id
@@ -28,28 +28,28 @@ resource "azurerm_app_service" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+  name                = "skttrsp20-vnet"
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
+  name                 = "skttrsp20-subnet"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "example" {
-  name                = "example-pip"
+  name                = "skttrsp20-pip"
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_application_gateway" "example" {
-  name                = "example-appgateway"
+  name                = "skttrsp20-appgateway"
   location            = var.location
   resource_group_name = var.resource_group_name
 
@@ -60,27 +60,27 @@ resource "azurerm_application_gateway" "example" {
   }
 
   gateway_ip_configuration {
-    name      = "example-gateway-ip-configuration"
+    name      = "skttrsp20-ip-configuration"
     subnet_id = azurerm_subnet.example.id
   }
 
   frontend_port {
-    name = "example-frontend-port"
+    name = "skttrsp20-frontend-port"
     port = 80
   }
 
   frontend_ip_configuration {
-    name                 = "example-frontend-ip-configuration"
+    name                 = "skttrsp20-ip-configuration"
     public_ip_address_id = azurerm_public_ip.example.id
   }
 
   backend_address_pool {
-    name         = "example-backend-address-pool"
+    name         = "skttrsp20-address-pool"
     ip_addresses = [azurerm_app_service.example[0].default_site_hostname, azurerm_app_service.example[1].default_site_hostname]
   }
 
   backend_http_settings {
-    name                  = "example-backend-http-settings"
+    name                  = "skttrsp20-backend-http-settings"
     cookie_based_affinity = "Disabled"
     path                  = "/"
     port                  = 80
@@ -89,17 +89,17 @@ resource "azurerm_application_gateway" "example" {
   }
 
 http_listener {
-    name                           = "example-http-listener"
-    frontend_ip_configuration_name = "example-frontend-ip-configuration"
-    frontend_port_name             = "example-frontend-port"
+    name                           = "skttrsp20-http-listener"
+    frontend_ip_configuration_name = "skttrsp20-frontend-ip-configuration"
+    frontend_port_name             = "skttrsp20-frontend-port"
     protocol                       = "Http"
  }
 request_routing_rule {
-    name                       = "example-request-routing-rule"
+    name                       = "skttrsp20-request-routing-rule"
     rule_type                  = "Basic"
-    http_listener_name         = "example-http-listener"
-    backend_address_pool_name  = "example-backend-address-pool"
-    backend_http_settings_name = "example-backend-http-settings"
+    http_listener_name         = "skttrsp20-http-listener"
+    backend_address_pool_name  = "skttrsp20-backend-address-pool"
+    backend_http_settings_name = "skttrsp20-backend-http-settings"
   }
 }
 #resource "azurerm_application_gateway_request_routing_rule" "example" {
