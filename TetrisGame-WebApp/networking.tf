ea704a9fa06a36c230cc66db3af3de75167836ce
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-30 09:12:10 +0000

    Updated networking.tf

--- TetrisGame-WebApp/networking.tf
+++ TetrisGame-WebApp/networking.tf
@@ -1,39 +1,129 @@
-resource "azurerm_virtual_network" "example" {
-  name                = "Tetris-apps-vnet"
-  address_space       = ["10.0.0.0/16"]
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
   location            = var.location
   resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
 }
-########
-resource "azurerm_subnet" "example" {
-  name                 = "Tetris-apps-subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.example.name
+
+# Create a subnet for the Application Gateway
+resource "azurerm_subnet" "subnet_gw" {
+  name                 = "mySubnet-gw"
+  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group_name
   address_prefixes     = ["10.0.1.0/24"]
 }
-########
-resource "azurerm_public_ip" "example" {
-  name                = "Tetris-apps-public-ip"
-  location            = var.location
+
+# Create a subnet for the Web Apps
+resource "azurerm_subnet" "subnet_web" {
+  name                 = "mySubnet-web"
+  resource_group_name  = azurerm_resource_group.rg.name
   resource_group_name = var.resource_group_name
-  allocation_method  = "Static"
-}
-########
-resource "azurerm_app_service_plan" "example" {
-  name                = "Tetris-apps-plan"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  kind                = "Linux"
-  
-  sku {
-    tier = "Standard"
-    size = "S1"
+  address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
   }
 }
-########
-resource "azurerm_app_service" "web_app" {
-  count               = 2
-  name                = "Tetris-${count.index + 1}"
+# Create a public IP address for the Application Gateway
+resource "azurerm_public_ip" "pip" {
+  name                = "myPublicIP"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+# Create an Application Gateway with WAF enabled
+resource "azurerm_application_gateway" "appgw" {
+  name                = "myAppGateway"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 2
+  }
+  gateway_ip_configuration {
+    name      = "myGatewayIPConfig"
+    subnet_id = azurerm_subnet.subnet_gw.id
+  }
+  frontend_port {
+    name = "port_80"
+    port = 80
+  }
+  frontend_ip_configuration {
+    name                 = "myFrontendIPConfig"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+  backend_address_pool {
+    name         = "myBackendAddressPool"
+    fqdns        = [azurerm_app_service.webapp1.default_site_hostname, azurerm_app_service.webapp2.default_site_hostname]
+    ip_addresses = []
+  }
+  backend_http_settings {
+    name                  = "myBackendHTTPSettings"
+    cookie_based_affinity = "Disabled"
+    path                  = "/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 20
+    probe_name                ="http-probe"
+}
+http_listener {
+  name                           ="http-listener"
+  frontend_ip_configuration_name ="myFrontendIPConfig"
+  frontend_port_name             ="port_80"
+  protocol                       ="Http"
+}
+request_routing_rule{
+  name                        ="http-rule"
+  rule_type                   ="Basic"
+  http_listener_name           ="http-listener"
+  backend_address_pool_name   ="myBackendAddressPool"
+  backend_http_settings_name   ="myBackendHTTPSettings"
+}
+probe{
+  name="http-probe"
+  protocol="Http"
+  path="/"
+  interval=30
+  timeout=30
+  unhealthy_threshold=3
+}
+ssl_policy{
+  policy_type="Predefined"
+  policy_name="AppGwSslPolicy20170401S"
+}
+  waf_configuration{
+  enabled=true
+  firewall_mode="Detection"
+  rule_set_type="OWASP"
+  rule_set_version="3.1"
+  file_upload_limit_mb=100
+  max_request_body_size_kb=128
+  request_body_check=true
+}
+}
+# Create an App Service Plan for the Web Apps
+resource "azurerm_app_service_plan" "plan" {
+  name                ="myAppServicePlan"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+sku{
+  tier="Standard"
+  size="S1"
+}
+  kind="Linux"
+  reserved=true
+}
+
+# Create the first Web App for Linux
+resource "azurerm_app_service" "webapp1" {
+name                ="webapp1-${random_string.rs.result}"
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = azurerm_app_service_plan.example.id
@@ -41,51 +131,33 @@ resource "azurerm_app_service" "web_app" {
   site_config {
     linux_fx_version = "DOCKER|skP20ContReg.azurecr.io/tetrisgameapp"
   }
+
 }
-########
-resource "azurerm_application_gateway" "example" {
-  name                = "web-apps-gateway"
-  resource_group_name = var.resource_group_name
+    identity{
+       type="SystemAssigned"
+
+    }
+}
+# Create the second Web App for Linux
+resource "azurerm_app_service" "webapp2" {
+  name                ="webapp2-${random_string.rs.result}"
   location            = var.location
-  sku {
-    name = "WAF_v2"
-    tier = "WAF_v2"
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    linux_fx_version = "DOCKER|skP20ContReg.azurecr.io/tetrisgameapp"
   }
 
-  gateway_ip_configuration {
-    name      = "my-gateway-ip-configuration"
-    subnet_id = azurerm_subnet.example.id
-  }
+}
+    identity{
+       type="SystemAssigned"
 
-  frontend_port {
-    name = "http"
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = "my-frontend-ip-configuration"
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
-
-  backend_address_pool {
-    count  = var.web_app_count
-    name   = "webapp-backend-pool-${count.index}"
-    fqdns  = [azurerm_app_service.web_app[count.index].default_site_hostname]
-  }
-
-  http_listener {
-    count                           = var.web_app_count
-    name                            = "http-listener-${count.index}"
-    frontend_ip_configuration_name = azurerm_application_gateway.example.frontend_ip_configuration[0].name
-    frontend_port_name              = "http"
-  }
-
-  request_routing_rule {
-    count                 = var.web_app_count
-    name                  = "webapp-rule-${count.index}"
-    rule_type             = "Basic"
-    http_listener_name    = azurerm_application_gateway.example.http_listener[count.index].name
-    backend_address_pool_name  = azurerm_application_gateway.example.backend_address_pool[count.index].name
-    backend_http_settings_name = "appGatewayBackendHttpSettings"
-  }
+    }
+}
+# Generate a random string for unique resource names
+resource "random_string" "rs" {
+    length=4
+    special=false
+    upper=false
 }
