169bdde3a757a885243feaff6df3295ec480b03b
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-01 09:07:24 +0000

    Deleted MultiCont-AKS-HealthApp

--- MultiCont-AKS-HealthApp/RG.tf
+++
@@ -1,7 +0,0 @@
-############################
-## Create ressource Group ##
-############################
-#resource "azurerm_resource_group" "psykprojs" {
-#  name     = var.resource_group_name
-#  location = var.location
-#}
