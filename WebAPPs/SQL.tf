eb78780aebb42aad51c66bff3e447eb40419d64f
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-01 13:45:53 +0000

    Updated SQL.tf

--- WebAPPs/SQL.tf
+++ WebAPPs/SQL.tf
@@ -1,7 +1,7 @@
 ###########################
 ## Create the SQL Server ##
 ###########################
-resource "azurerm_sql_server" "sql_backend_pool" {
+resource "azurerm_sql_server" "example" {
   name                         = "multiwebsqlserv"
   resource_group_name          = var.resource_group_name
   location                     = var.location
@@ -9,46 +9,13 @@ resource "azurerm_sql_server" "sql_backend_pool" {
   administrator_login          = var.admin_username
   administrator_login_password = var.admin_password
 }
+
 ##############################
-## Create the SQL databases ##
+## Create the SQL database ##
 ##############################
-resource "azurerm_sql_database" "sql_backend_pool" {
-  count               = 3
-  name                = "sqldb-${count.index}"
+resource "azurerm_sql_database" "example" {
+  name                = "sqldb"
   resource_group_name = var.resource_group_name
   location            = var.location
-  server_name         = azurerm_sql_server.sql_backend_pool.name
-}
-
-######################################
-## Create the load balancer for SQL ##
-######################################
-resource "azurerm_lb" "sqldbbkndlb" {
-  name                = "sqldbbkndlb"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = "Standard"
-
-  frontend_ip_configuration {
-    name                          = "PrivateIPAddress"
-    private_ip_address_allocation = "Dynamic"
-    private_ip_address_version    = "IPv4"
-    subnet_id                     = azurerm_subnet.example.id # Update this to reference an existing subnet resource
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "example" {
-  loadbalancer_id = azurerm_lb.sqldbbkndlb.id
-  name            = "example-backend-pool"
-}
-
-####################################################################
-## Add the SQL databases to the backend pool of the load balancer ##
-####################################################################
-resource "azurerm_lb_backend_address_pool_address" "sql_backend_pool" {
-  count                   = length(azurerm_sql_database.sql_backend_pool)
-  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id # Add this argument
-  virtual_network_id      = azurerm_virtual_network.example.id
-  name                    = "sql_backend_pool-${count.index}"
-  ip_address              = azurerm_sql_database.sql_backend_pool[count.index].id # Update this argument
+  server_name         = azurerm_sql_server.example.name
 }
