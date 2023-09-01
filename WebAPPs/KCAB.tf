b221c31446f9124091c9e2cc3d9d9d02c1d8d4fb
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-01 09:41:35 +0000

    Updated KCAB.tf

--- WebAPPs/KCAB.tf
+++ WebAPPs/KCAB.tf
@@ -24,37 +24,63 @@ resource "azurerm_app_service_plan" "asp" {
 # Create a web app
 resource "azurerm_app_service" "webapp" {
   name                = "skskabdocker-prd"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  app_service_plan_id  = azurerm_app_service_plan.asp.id
-  site_config {
-    linux_fx_version = "NODE|14-lts"
-  }
-}
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
-# Create a dev web app
-resource "azurerm_app_service" "dev_webapp" {
-  name                = "skskabdocker-dev"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  app_service_plan_id  = azurerm_app_service_plan.asp.id
   site_config {
-    linux_fx_version = "NODE|14-lts"
-  }
-}
-
-# Link the web app to the container image
-resource "azurerm_app_service" "container" {
-  name                = azurerm_app_service.webapp.name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  app_service_plan_id  = azurerm_app_service_plan.asp.id
-  site_config {
-    always_on       = true
+    always_on = true
     linux_fx_version = "DOCKER|psykali/stackoverp20kcab"
   }
 
-  app_settings = {
-    DOCKER_REGISTRY_SERVER_URL = "https://index.docker.io"
+  identity {
+    type = "SystemAssigned"
   }
+
+  tags = local.common_tags
 }
+
+resource "azurerm_app_service_slot" "example" {
+  app_service_name       = azurerm_app_service.wordpress[0].name
+  location               = azurerm_app_service.wordpress[0].location
+  resource_group_name    = azurerm_app_service.wordpress[0].resource_group_name
+  app_service_plan_id    = azurerm_app_service_plan.example.id
+  name                   = "staging"
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLAzure"
+    value = "Server=tcp:${azurerm_lb.sqldbbkndlb.private_ip_address},1433;Initial Catalog=sqldb-0;User ID=${var.admin_username};Password=${var.admin_password};"
+  }
+# Create a dev web app
+resource "azurerm_app_service" "dev_webapp" {
+  name                = "skskabdocker-dev"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    always_on = true
+    linux_fx_version = "DOCKER|psykali/stackoverp20kcab"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.common_tags
+}
+
+resource "azurerm_app_service_slot" "example" {
+  app_service_name       = azurerm_app_service.wordpress[0].name
+  location               = azurerm_app_service.wordpress[0].location
+  resource_group_name    = azurerm_app_service.wordpress[0].resource_group_name
+  app_service_plan_id    = azurerm_app_service_plan.example.id
+  name                   = "staging"
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLAzure"
+    value = "Server=tcp:${azurerm_lb.sqldbbkndlb.private_ip_address},1433;Initial Catalog=sqldb-0;User ID=${var.admin_username};Password=${var.admin_password};"
+  }
+
