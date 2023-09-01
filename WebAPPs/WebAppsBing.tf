594cd8128dddeba6c775c76f1ad1d034ad433eb7
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-01 12:49:11 +0000

    Updated WebAppsBing.tf

--- WebAPPs/WebAppsBing.tf
+++ WebAPPs/WebAppsBing.tf
@@ -13,6 +13,7 @@ resource "azurerm_subnet" "example" {
 }
 
 resource "azurerm_public_ip" "example" {
+  name                         = "multiwebip"
   resource_group_name          = var.resource_group_name
   location                     = var.location
   allocation_method   = "Static"
