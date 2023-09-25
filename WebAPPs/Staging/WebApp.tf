6fdf94e06057b52162d8947cb25ddf82ab3ecd01
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-25 16:41:56 +0000

    Updated WebApp.tf

--- WebAPPs/Staging/WebApp.tf
+++ WebAPPs/Staging/WebApp.tf
@@ -1,99 +1,88 @@
-# Create an Application Insights instance
-resource "azurerm_application_insights" "appinsights" {
-  name                = "myappinsights"
+resource "azurerm_application_insights" "skprjs_ai" {
+  name                = "Sofstaging-ai"
   location            = var.location
   resource_group_name = var.resource_group_name
+  application_type    = "web"
 }
 
-# Create a MySQL server for the databases
-resource "azurerm_mysql_server" "mysql_server" {
-  name                = "my-mysql-server"
+resource "azurerm_application_insights_web_test" "skprjs_appinsights" {
+  name                = "SofStaging-ai"
+  resource_group_name = var.resource_group_name
+  application_insights_id = azurerm_application_insights.skprjs_ai.id
+  location       = azurerm_application_insights.skprjs_ai.location
+  kind                    = "ping"
+  frequency               = 300
+  timeout                 = 60
+  enabled                 = true
+  geo_locations           = ["us-tx-sn1-azr", "us-il-ch1-azr"]
+
+  configuration = <<XML
+<WebTest Name="WebTest1" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
+  <Items>
+    <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="http://microsoft.com" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+  </Items>
+</WebTest>
+XML
+
+}
+
+output "webtest_id" {
+  value = azurerm_application_insights_web_test.skprjs_appinsights.id
+}
+
+output "webtests_synthetic_id" {
+  value = azurerm_application_insights_web_test.skprjs_appinsights.synthetic_monitor_id
+}
+
+
+resource "azurerm_app_service_plan" "skprjs_asp" {
+  name                = "Sof-asp"
   location            = var.location
   resource_group_name = var.resource_group_name
-
-  sku_name   = "B_Gen5_1"
-  storage_mb = 5120
-
-  version = "5.7"
-  administrator_login          = var.admin_username
-  administrator_login_password = var.admin_password
-}
-
-# Create a MySQL database for every web app
-resource "azurerm_mysql_database" "mysql_database" {
-  count               = 3
-  name                = "wordpress-db-${count.index}"
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_mysql_server.mysql_server.name
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
-}
-
-# Create Web Apps and App Service Plans
-resource "azurerm_app_service_plan" "webapp_asp" {
-  count               = 3
-  name                = "webapp-asp-${count.index}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  kind                = "Linux"
+  reserved            = true
   sku {
     tier = "Standard"
     size = "S1"
   }
 }
 
-resource "azurerm_app_service" "webapp" {
-  count               = 3
-  name                = "webapp-${count.index}"
+
+resource "azurerm_app_service" "tetris_webapps" {
+  count               = 1
+  name                = "SofStaging-${count.index}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_app_service_plan.webapp_asp[count.index].id
+  app_service_plan_id = azurerm_app_service_plan.skprjs_asp.id
 
   site_config {
-    linux_fx_version = "DOCKER|${var.docker_registry_server_url}/prd/stackoverp20kcab"
-    always_on        = true
-
-    docker_registry_server_url      = var.docker_registry_server_url
-    docker_registry_server_user     = var.docker_registry_server_user
-    docker_registry_server_password = var.docker_registry_server_password
+    linux_fx_version = "DOCKER|skP20ContReg.azurecr.io/tetrisgameapp"
   }
 
   app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.appinsights.instrumentation_key
-    "DATABASE_NAME"                 = var.db_name
+    "WEBSITES_PORT" = "80"
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.skprjs_ai.instrumentation_key
+     "DATABASE_NAME"                 = var.db_name
     "MYSQL_HOST"                    = var.db_host
     "MYSQL_PORT"                    = "1433"
     "MYSQL_USER"                    = var.admin_username
     "MYSQL_PASSWORD"                = var.admin_password
   }
 
-
   identity {
     type = "SystemAssigned"
   }
+
+  tags = local.common_tags
 }
 
-# Create an App Insights resource per Web App
-resource "azurerm_app_insights" "appinsights_app" {
-  count               = 3
-  name                = "app-${count.index}-insights"
-  resource_group_name = var.resource_group_name
-  application_id      = azurerm_application_insights.appinsights.application_id
-  application_type    = "web"
+resource "azurerm_app_service_slot" "staging" {
+  count               = 1
+  name                = "slot"
+  app_service_name    = azurerm_app_service.skprjs_webapps[count.index].name
+  location            = azurerm_app_service.skprjs_webapps[count.index].location
+  resource_group_name = azurerm_app_service.skprjs_webapps[count.index].resource_group_name
+  app_service_plan_id = azurerm_app_service_plan.skprjs_asp.id
 
-  location       = azurerm_application_insights.appinsights.location
-  tags           = local.common_tags
-  correlation {
-    client_track_enabled = false
-  }
-  web {
-    app_id = azurerm_app_service.webapp[count.index].id
-  }
-}
-
-# Output the Web App URLs
-output "webapp_urls" {
-  value = [
-    for i in range(0, 3):
-    "https://${azurerm_app_service.webapp[i].default_site_hostname}/"
-  ]
+  tags = local.common_tags
 }
