f7e3a7ab910365c44e8a5616513255288dfc48c4
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-25 16:28:07 +0000

    Updated Variables.tf

--- WebAPPs/Staging/Variables.tf
+++ WebAPPs/Staging/Variables.tf
@@ -26,3 +26,19 @@ variable "admin_password" {
   type        = string
   default = "V83phJJDRExKW3kmhLCm4"
 }
+#######
+variable "docker_registry_server_url" {
+  description = "Value of the Admin Pass"
+  type        = string
+  default = "skp20contreg.azurecr.io"
+}
+variable "docker_registry_server_user" {
+  description = "Value of the Admin Pass"
+  type        = string
+  default = "skP20ContReg"
+}
+variable "docker_registry_server_password" {
+  description = "Value of the Admin Pass"
+  type        = string
+  default = "y+vwH2D7QqUE3VHrBTz+hsMAUejMduPjug7E40Alau+ACRCytZmV"
+}
