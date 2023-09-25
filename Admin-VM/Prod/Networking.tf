e66c57b9d07f7faf3169c7a91c92a0825f4b9fd7
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-25 17:36:28 +0000

    Updated Networking.tf

--- Admin-VM/Prod/Networking.tf
+++ Admin-VM/Prod/Networking.tf
@@ -2,13 +2,13 @@
 ## Networking ##
 ################
 # Creat Vnet
-#resource "azurerm_virtual_network" "vnet" {
-#  name                = var.virtual_network_name
-#  location            = var.location
-#  resource_group_name = var.resource_group_name
-#  address_space       = ["10.0.0.0/8"]
-#  tags = local.common_tags
-#}
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/8"]
+  tags = local.common_tags
+}
 
 # Create Subnet
 resource "azurerm_subnet" "default" {
