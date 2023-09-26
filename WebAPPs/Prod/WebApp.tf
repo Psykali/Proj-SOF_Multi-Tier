983dc667255e30e491dd7745a76409266b2ee118
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-26 10:08:34 +0000

    Updated WebApp.tf

--- WebAPPs/Prod/WebApp.tf
+++ WebAPPs/Prod/WebApp.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_plan" "skprjs_asp" {
+resource "azurerm_service_plan" "skprjs_asp" {
   name                = "sofprd-asp"
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -10,13 +10,12 @@ resource "azurerm_app_service_plan" "skprjs_asp" {
   }
 }
 
-
 resource "azurerm_app_service" "skprjs_webapps" {
   count               = 3
   name                = "sofprd-${count.index}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_app_service_plan.skprjs_asp.id
+  app_service_plan_id = azurerm_service_plan.skprjs_asp.id
 
   site_config {
     linux_fx_version = "DOCKER|skP20ContReg.azurecr.io/prd/stackoverp20kcab"
@@ -45,7 +44,7 @@ resource "azurerm_app_service_slot" "staging" {
   app_service_name    = azurerm_app_service.skprjs_webapps[count.index].name
   location            = azurerm_app_service.skprjs_webapps[count.index].location
   resource_group_name = azurerm_app_service.skprjs_webapps[count.index].resource_group_name
-  app_service_plan_id = azurerm_app_service_plan.skprjs_asp.id
+  app_service_plan_id = azurerm_service_plan.skprjs_asp.id
 
   tags = local.common_tags
 }
