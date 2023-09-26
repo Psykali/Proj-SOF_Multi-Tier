58a42217532464db51bedbd131574aaf8694ea38
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-26 09:36:04 +0000

    Updated ACI.tf

--- WebAPPs/Staging/ACI.tf
+++ WebAPPs/Staging/ACI.tf
@@ -1,32 +1,31 @@
-resource "azurerm_container_group" "skprjs_aci" {
-  name                = "SofStaging-aci"
+resource "azurerm_container_group" "skprjs_container" {
+  name                = "Sofstaging-container"
   location            = var.location
   resource_group_name = var.resource_group_name
-  ip_address_type     = "public"
-  dns_name_label      = "SofStaging-aci"
-  os_type             = "Linux"
+
+  os_type = "Linux"
 
   container {
-    name   = "SofStaging-aci"
+    name   = "skprjs-webapp"
     image  = "skP20ContReg.azurecr.io/dev/stackoverp20kcab"
     cpu    = "1.0"
     memory = "1.5"
-
-    ports {
-      port     = 80
+    port {
       protocol = "TCP"
-    }
-
-    environment_variables = {
-      "WEBSITES_PORT"                   = "80"
-      "APPINSIGHTS_INSTRUMENTATIONKEY"  = azurerm_application_insights.skprjs_ai.instrumentation_key
-      "DATABASE_NAME"                   = var.db_name
-      "MYSQL_HOST"                      = var.db_host
-      "MYSQL_PORT"                      = "1433"
-      "MYSQL_USER"                      = var.admin_username
-      "MYSQL_PASSWORD"                  = var.admin_password
+      port     = 80
     }
   }
 
   tags = local.common_tags
+
+  diagnostics {
+    log_analytics {
+      workspace_id = azurerm_log_analytics_workspace.skprjs_la.id
+    }
+
+    # Enable Application Insights for monitoring ACI
+    app_insights {
+      instrumentation_key = azurerm_application_insights.skprjs_ai.instrumentation_key
+    }
+  }
 }
