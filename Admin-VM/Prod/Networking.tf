9e59f9ce31bb25166e329117851ab5a30b5b6bf4
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-27 13:11:57 +0000

    Updated Networking.tf

--- Admin-VM/Prod/Networking.tf
+++ Admin-VM/Prod/Networking.tf
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "bastion" {
   name                 = "AzureBastionSubnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = var.resource_group_name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 
