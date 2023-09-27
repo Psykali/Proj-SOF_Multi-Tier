235d73f2748a90cff9c6503cf0d26694b0fe4dce
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-27 07:42:25 +0000

    Added RG.tf

---
+++ ContReg/RG.tf
@@ -0,0 +1,7 @@
+###############################
+## Create the Resource Group ##
+###############################
+##resource "azurerm_resource_group" "rg" {
+##  name     = var.resource_group_name
+##  location = var.location_rg
+##}
