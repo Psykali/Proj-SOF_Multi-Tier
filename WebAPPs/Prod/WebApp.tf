d61a4d1df368f515fe097740eb9123497eabd6b1
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-27 08:13:25 +0000

    Updated WebApp.tf

--- WebAPPs/Prod/WebApp.tf
+++ WebAPPs/Prod/WebApp.tf
@@ -43,7 +43,7 @@ resource "azurerm_app_service" "skprjs_webapps" {
 ###########
 resource "azurerm_app_service_slot" "staging" {
   count               = 3
-  name                = "slot"
+  name                = "staging"
   app_service_name    = azurerm_app_service.skprjs_webapps[count.index].name
   location            = azurerm_app_service.skprjs_webapps[count.index].location
   resource_group_name = azurerm_app_service.skprjs_webapps[count.index].resource_group_name
