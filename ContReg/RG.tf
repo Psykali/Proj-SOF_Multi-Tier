5981457d2034a24d4d11b35645ee8a4b54c97c4b
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-27 07:44:56 +0000

    Updated RG.tf

--- ContReg/RG.tf
+++ ContReg/RG.tf
@@ -1,7 +1,20 @@
-###############################
-## Create the Resource Group ##
-###############################
+###########################
+## Create Resource Group
+##########################
 ##resource "azurerm_resource_group" "rg" {
 ##  name     = var.resource_group_name
-##  location = var.location_rg
+##  location = var.location
 ##}
+##########
+## Tags ##
+##########
+locals {
+  common_tags = {
+    CreatedBy = "SK"
+    Env       = "Prod"
+    Why       = "DipP20"
+    Proj        = "Proj-Multitier"
+    Infratype   = "PaaS-IaaS-IaC"
+    Ressources  = "VM-WebApp-NSG-VNET-ContReg-Workbook-DockerImg"
+  }
+}
