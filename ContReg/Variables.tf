75868682deb0e3b06ca4c587baaeaa81ff50c7cb
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-27 07:38:48 +0000

    Added Variables.tf

---
+++ ContReg/Variables.tf
@@ -0,0 +1,20 @@
+#############
+## Vars RG ##
+#############
+variable "resource_group_name" {
+  description = "PERSO_SIEF"
+}
+
+variable "location_rg" {
+  description = "francecentral"
+}
+##################
+## Vars ContReg ##
+##################
+variable "location_contreg" {
+  description = "westeurope"
+}
+
+##variable "contreg_name" {
+##  description = "skP20ContReg"
+##}
