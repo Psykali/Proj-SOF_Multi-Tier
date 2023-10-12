bdf3d329b7bc806b5db04fccb740e817fa7d6353
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-10-12 12:59:51 +0000

    Updated WebApp.tf

--- WebAPPs/Dev/WebApp.tf
+++ WebAPPs/Dev/WebApp.tf
@@ -38,16 +38,3 @@ resource "azurerm_app_service" "skprjs_webapps" {
 
   tags = local.common_tags
 }
-###########
-## Slots ##
-###########
-resource "azurerm_app_service_slot" "staging" {
-  count               = 1
-  name                = "slot"
-  app_service_name    = azurerm_app_service.skprjs_webapps[count.index].name
-  location            = azurerm_app_service.skprjs_webapps[count.index].location
-  resource_group_name = azurerm_app_service.skprjs_webapps[count.index].resource_group_name
-  app_service_plan_id = azurerm_service_plan.skprjs_asp.id
-
-  tags = local.common_tags
-}
