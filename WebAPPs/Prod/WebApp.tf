7930c5c594411381c94a34937bf21ade2414c123
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-10-12 13:04:24 +0000

    Updated WebApp.tf

--- WebAPPs/Prod/WebApp.tf
+++ WebAPPs/Prod/WebApp.tf
@@ -2,7 +2,8 @@
 ## Service App Plan ##
 ######################
 resource "azurerm_service_plan" "skprjs_asp" {
-  name                = "sofprd-asp"
+  count               = 1
+  name                = "sofprdwe-asp-${count.index}"
   location            = var.location
   resource_group_name = var.resource_group_name
   sku_name = "S1"
@@ -12,8 +13,8 @@ resource "azurerm_service_plan" "skprjs_asp" {
 ## WebApps ##
 #############
 resource "azurerm_app_service" "skprjs_webapps" {
-  count               = 3
-  name                = "sofprd-${count.index}"
+  count               = 1
+  name                = "sofprdwe-${count.index}"
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = azurerm_service_plan.skprjs_asp.id
@@ -42,12 +43,66 @@ resource "azurerm_app_service" "skprjs_webapps" {
 ## Slots ##
 ###########
 resource "azurerm_app_service_slot" "staging" {
-  count               = 3
+  count               = 1
   name                = "staging"
   app_service_name    = azurerm_app_service.skprjs_webapps[count.index].name
   location            = azurerm_app_service.skprjs_webapps[count.index].location
   resource_group_name = azurerm_app_service.skprjs_webapps[count.index].resource_group_name
   app_service_plan_id = azurerm_service_plan.skprjs_asp.id
+
+  tags = local.common_tags
+}
+######################
+## Service App Plan ##
+######################
+resource "azurerm_service_plan" "skprjs_asp2" {
+  count               = 1
+  name                = "sofprdfc-asp-${count.index}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku_name = "S1"
+  os_type  = "Linux"
+}
+#############
+## WebApps ##
+#############
+resource "azurerm_app_service" "skprjs_webapps2" {
+  count               = 2
+  name                = "sofprdfc-${count.index}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = azurerm_service_plan.skprjs_asp2.id
+
+  site_config {
+    linux_fx_version = "DOCKER|skP20ContReg.azurecr.io/prd/stackoverp20kcab"
+  }
+
+  app_settings = {
+    "WEBSITES_PORT" = "80"
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.skprjs_ai.instrumentation_key
+     "DATABASE_NAME"                 = var.db_name
+    "MYSQL_HOST"                    = var.db_host
+    "MYSQL_PORT"                    = "1433"
+    "MYSQL_USER"                    = var.admin_username
+    "MYSQL_PASSWORD"                = var.admin_password
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.common_tags
+}
+###########
+## Slots ##
+###########
+resource "azurerm_app_service_slot" "staging2" {
+  count               = 1
+  name                = "staging"
+  app_service_name    = azurerm_app_service.skprjs_webapps2[count.index].name
+  location            = azurerm_app_service.skprjs_webapps2[count.index].location
+  resource_group_name = azurerm_app_service.skprjs_webapps2[count.index].resource_group_name
+  app_service_plan_id = azurerm_service_plan.skprjs_asp2.id
 
   tags = local.common_tags
 }
