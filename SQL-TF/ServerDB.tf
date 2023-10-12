8a8ef142e0198125b5e5a4bb13eb1d4633fe61d2
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-10-12 12:59:22 +0000

    Updated ServerDB.tf

--- SQL-TF/ServerDB.tf
+++ SQL-TF/ServerDB.tf
@@ -2,7 +2,7 @@
 ## Create the SQL Server ##
 ###########################
 resource "azurerm_sql_server" "example" {
-  name                         = "skdb01"
+  name                         = "sofdbservfc"
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = "12.0"
@@ -10,6 +10,15 @@ resource "azurerm_sql_server" "example" {
   administrator_login_password = var.admin_password
 }
 
+##############################
+## Create the SQL database ##
+##############################
+resource "azurerm_sql_database" "staging" {
+  name                = "q2adev"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  server_name         = azurerm_sql_server.example.name
+}
 ##############################
 ## Create the SQL database ##
 ##############################
