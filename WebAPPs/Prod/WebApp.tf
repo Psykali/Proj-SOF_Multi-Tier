4801581dfa6b3a211cfe3d16d1b5f9a52a207d4d
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-11-01 19:14:04 +0000

    Updated WebApp.tf

--- WebAPPs/Prod/WebApp.tf
+++ WebAPPs/Prod/WebApp.tf
@@ -48,7 +48,7 @@ resource "azurerm_app_service_slot" "staging" {
   app_service_name    = azurerm_app_service.skprjs_webapps[count.index].name
   location            = azurerm_app_service.skprjs_webapps[count.index].location
   resource_group_name = azurerm_app_service.skprjs_webapps[count.index].resource_group_name
-  app_service_plan_id = azurerm_service_plan.skprjs_asp.id
+  app_service_plan_id = azurerm_service_plan.skprjs_asp[count.index].id
 
   tags = local.common_tags
 }
@@ -102,7 +102,7 @@ resource "azurerm_app_service_slot" "staging2" {
   app_service_name    = azurerm_app_service.skprjs_webapps2[count.index].name
   location            = azurerm_app_service.skprjs_webapps2[count.index].location
   resource_group_name = azurerm_app_service.skprjs_webapps2[count.index].resource_group_name
-  app_service_plan_id = azurerm_service_plan.skprjs_asp2.id
+  app_service_plan_id = azurerm_service_plan.skprjs_asp2[count.index].id
 
   tags = local.common_tags
 }
